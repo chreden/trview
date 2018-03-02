f30d7a381018821310be8791fabfb97ba94527bf
Chris Eden <4263940+chreden@users.noreply.github.com>
2018-03-02 16:43:32 +0000

    Start on room navigator
    It isn't finished

--- trview/RoomNavigator.cpp
+++ trview/RoomNavigator.cpp
@@ -1,7 +1,162 @@
 #include "stdafx.h"
 #include "RoomNavigator.h"
+#include "ITextureStorage.h"
+
+#include <trview.ui/GroupBox.h>
+#include <trview.ui/Label.h>
+#include <trview.ui/NumericUpDown.h>
+#include <trview.ui/Button.h>
+#include <trview.ui/StackPanel.h>
 
 namespace trview
 {
-    
+    RoomNavigator::RoomNavigator(ui::Control& parent, const ITextureStorage& texture_storage)
+    {
+        using namespace ui;
+
+        auto rooms_groups = std::make_unique<GroupBox>(
+            Point(0, 0),
+            Size(140, 130),
+            Colour(1.0f, 0.5f, 0.5f, 0.5f),
+            Colour(1.0f, 0.0f, 0.0f, 0.0f),
+            L"Rooms");
+
+        auto room_highlight = std::make_unique<Button>(
+            Point(12, 20),
+            Size(16, 16),
+            texture_storage.untextured(),
+            texture_storage.untextured());
+
+        auto room_highlight_label = std::make_unique<Label>(
+            Point(32, 20),
+            Size(40, 16),
+            Colour(1.0f, 0.5f, 0.5f, 0.5f),
+            L"Highlight",
+            10.0f,
+            TextAlignment::Left,
+            ParagraphAlignment::Centre);
+
+        // room_highlight->on_click += [&]() { toggle_highlight(); };
+        // _room_highlight = room_highlight.get();
+
+        auto room_box = std::make_unique<GroupBox>(
+            Point(12, 40),
+            Size(120, 80),
+            Colour(1.0f, 0.5f, 0.5f, 0.5f),
+            Colour(1.0f, 0.0f, 0.0f, 0.0f),
+            L"Room");
+
+        auto room_controls = std::make_unique<StackPanel>(
+            Point(12, 12),
+            Size(96, 60),
+            Colour(1.f, 0.5f, 0.5f, 0.5f),
+            Size(0, 0),
+            StackPanel::Direction::Vertical);
+
+        auto room_number_labels = std::make_unique<StackPanel>(
+            Point(0, 0),
+            Size(96, 30),
+            Colour(1.0f, 0.5f, 0.5f, 0.5f),
+            Size(5, 5),
+            StackPanel::Direction::Horizontal);
+
+        auto room_number = std::make_unique<NumericUpDown>(
+            Point(90, 16),
+            Size(40, 20),
+            Colour(1.0f, 0.4f, 0.4f, 0.4f),
+            texture_storage.untextured(),
+            texture_storage.untextured(),
+            0,
+            0);
+
+        auto room_number_label = std::make_unique<Label>(
+            Point(0, 0),
+            Size(8, 16),
+            Colour(1.0f, 0.5f, 0.5f, 0.5f),
+            L"/",
+            16.f,
+            TextAlignment::Left,
+            ParagraphAlignment::Centre);
+
+        auto room_max_label = std::make_unique<Label>(
+            Point(0, 0),
+            Size(40, 20),
+            Colour(1.0f, 0.4f, 0.4f, 0.4f),
+            L"0",
+            10.f,
+            TextAlignment::Centre,
+            ParagraphAlignment::Centre);
+
+        // _room_current_room = room_number.get();
+        // _room_rooms = room_max_label.get();
+        // 
+        // room_number->on_value_changed += [&](int32_t value)
+        // {
+        //     select_room(value);
+        // };
+
+        room_number_labels->add_child(std::move(room_number));
+        room_number_labels->add_child(std::move(room_number_label));
+        room_number_labels->add_child(std::move(room_max_label));
+
+        auto room_info_labels = std::make_unique<StackPanel>(
+            Point(12, 12),
+            Size(96, 36),
+            Colour(1.f, 0.5f, 0.5f, 0.5f),
+            Size(5, 5),
+            StackPanel::Direction::Horizontal);
+
+        auto room_x_label = std::make_unique<Label>(
+            Point(0, 0),
+            Size(40, 16),
+            Colour(1.0f, 0.5f, 0.5f, 0.5f),
+            L"X:",
+            10.f,
+            TextAlignment::Left,
+            ParagraphAlignment::Centre);
+
+        auto room_z_label = std::make_unique<Label>(
+            Point(0, 0),
+            Size(40, 16),
+            Colour(1.0f, 0.5f, 0.5f, 0.5f),
+            L"Z:",
+            10.f,
+            TextAlignment::Left,
+            ParagraphAlignment::Centre);
+
+        _room_x = room_x_label.get();
+        _room_z = room_z_label.get();
+
+        room_info_labels->add_child(std::move(room_x_label));
+        room_info_labels->add_child(std::move(room_z_label));
+
+        room_controls->add_child(std::move(room_number_labels));
+        room_controls->add_child(std::move(room_info_labels));
+
+        room_box->add_child(std::move(room_controls));
+
+        rooms_groups->add_child(std::move(room_highlight));
+        rooms_groups->add_child(std::move(room_highlight_label));
+        rooms_groups->add_child(std::move(room_box));
+
+        parent.add_child(std::move(rooms_groups));
+    }
+
+    void RoomNavigator::set_room_info(RoomInfo room_info)
+    {
+        // Update the labels.
+    }
+
+    void RoomNavigator::set_max_rooms(uint32_t max_rooms)
+    {
+
+    }
+
+    void RoomNavigator::set_highlight(bool highlighted)
+    {
+    }
+
+    void RoomNavigator::set_selected_room(uint32_t selected_room)
+    {
+    }
 }
--- trview/RoomNavigator.h
+++ trview/RoomNavigator.h
@@ -1,13 +1,35 @@
 #pragma once
 
+#include <cstdint>
+#include <trview.common/Event.h>
+#include "RoomInfo.h"
+
 namespace trview
 {
-    // The room navigator window allows the user to select which room to look at.
+    namespace ui
+    {
+        class Control;
+        class Label;
+    }
+
+    struct ITextureStorage;
+
+    // The room navigator control allows the user to select which room to look at.
     class RoomNavigator
     {
     public:
+        RoomNavigator(ui::Control& parent, const ITextureStorage& texture_storage);
 
+        Event<uint32_t> on_room_selected;
+        Event<bool> on_highlight;
+
+        void set_room_info(RoomInfo room_info);
+        void set_max_rooms(uint32_t max_rooms);
+        void set_highlight(bool highlighted);
+        void set_selected_room(uint32_t selected_room);
     private:
-
+        ui::Label* _max_rooms;
+        ui::Label* _room_x;
+        ui::Label* _room_z;
     };
 }
