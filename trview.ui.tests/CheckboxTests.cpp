4cd63f56d2e52bdefc8791bc111fbcec8cae2503
Chris Eden <4263940+chreden@users.noreply.github.com>
2018-03-26 21:48:41 +0100

    Make test project
    Has empty test

---
+++ trview.ui.tests/CheckboxTests.cpp
@@ -0,0 +1,23 @@
+#include "CppUnitTest.h"
+
+#include <trview.ui/Checkbox.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace trview
+{
+    namespace ui
+    {
+        namespace tests
+        {
+            TEST_CLASS(CheckboxTests)
+            {
+            public:
+                TEST_METHOD(StateChangeEventNotRaised)
+                {
+                    
+                }
+            };
+        }
+    }
+}
