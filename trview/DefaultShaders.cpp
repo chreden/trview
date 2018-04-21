05b516780eb5a03facf439539fd8703b5a31d7f1
Chris Eden <4263940+chreden@users.noreply.github.com>
2018-04-21 13:49:20 +0100

    Load UI shaders but don't use them yet

--- trview/DefaultShaders.cpp
+++ trview/DefaultShaders.cpp
@@ -10,26 +10,52 @@
 
 namespace trview
 {
+    namespace
+    {
+        void load_level_shaders(const CComPtr<ID3D11Device>& device, IShaderStorage& storage)
+        {
+            std::vector<D3D11_INPUT_ELEMENT_DESC> input_desc(3);
+            memset(&input_desc[0], 0, sizeof(D3D11_INPUT_ELEMENT_DESC) * input_desc.size());
+            input_desc[0].SemanticName = "Position";
+            input_desc[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+            input_desc[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+
+            input_desc[1].SemanticName = "Texcoord";
+            input_desc[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+            input_desc[1].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+            input_desc[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+
+            input_desc[2].SemanticName = "Texcoord";
+            input_desc[2].SemanticIndex = 1;
+            input_desc[2].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+            input_desc[2].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+            input_desc[2].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+
+            storage.add("level_vertex_shader", std::make_unique<VertexShader>(device, load_file(L"level_vertex_shader.cso"), input_desc));
+            storage.add("level_pixel_shader", std::make_unique<PixelShader>(device, load_file(L"level_pixel_shader.cso")));
+        }
+
+        void load_ui_shaders(const CComPtr<ID3D11Device>& device, IShaderStorage& storage)
+        {
+            std::vector<D3D11_INPUT_ELEMENT_DESC> input_desc(2);
+            memset(&input_desc[0], 0, sizeof(D3D11_INPUT_ELEMENT_DESC) * input_desc.size());
+            input_desc[0].SemanticName = "Position";
+            input_desc[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+            input_desc[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+
+            input_desc[1].SemanticName = "Texcoord";
+            input_desc[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+            input_desc[1].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+            input_desc[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+
+            storage.add("ui_vertex_shader", std::make_unique<VertexShader>(device, load_file(L"ui_vertex_shader.cso"), input_desc));
+            storage.add("ui_pixel_shader", std::make_unique<PixelShader>(device, load_file(L"ui_pixel_shader.cso")));
+        }
+    }
+
     void load_default_shaders(const CComPtr<ID3D11Device>& device, IShaderStorage& storage)
     {
-        std::vector<D3D11_INPUT_ELEMENT_DESC> input_desc(3);
-        memset(&input_desc[0], 0, sizeof(D3D11_INPUT_ELEMENT_DESC) * input_desc.size());
-        input_desc[0].SemanticName = "Position";
-        input_desc[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-        input_desc[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
-
-        input_desc[1].SemanticName = "Texcoord";
-        input_desc[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-        input_desc[1].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
-        input_desc[1].Format = DXGI_FORMAT_R32G32_FLOAT;
-
-        input_desc[2].SemanticName = "Texcoord";
-        input_desc[2].SemanticIndex = 1;
-        input_desc[2].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-        input_desc[2].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
-        input_desc[2].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-
-        storage.add("level_vertex_shader", std::make_unique<VertexShader>(device, load_file(L"level_vertex_shader.cso"), input_desc));
-        storage.add("level_pixel_shader", std::make_unique<PixelShader>(device, load_file(L"level_pixel_shader.cso")));
+        load_level_shaders(device, storage);
+        load_ui_shaders(device, storage);
     }
 }
