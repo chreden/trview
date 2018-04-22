500529bf4887c94244a23c487960af038c68ef89
Chris Eden <4263940+chreden@users.noreply.github.com>
2018-04-22 16:20:39 +0100

    Graphics unit tests

---
+++ trview.graphics.tests/ShaderStorageTests.cpp
@@ -0,0 +1,19 @@
+#include "CppUnitTest.h"
+
+#include <trview.graphics/ShaderStorage.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace trview
+{
+    namespace ui
+    {
+        namespace tests
+        {
+            TEST_CLASS(ShaderStorageTests)
+            {
+            public:
+            };
+        }
+    }
+}
