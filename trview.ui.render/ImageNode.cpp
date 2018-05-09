d29a7b86009b85112927265a51184eb5cbd5f349
Chris Eden <4263940+chreden@users.noreply.github.com>
2018-05-09 20:40:15 +0100

    Images are back

--- trview.ui.render/ImageNode.cpp
+++ trview.ui.render/ImageNode.cpp
@@ -22,7 +22,7 @@ namespace trview
             {
                 WindowNode::render_self(context, sprite);
                 RenderTargetStore store(context);
-                context->OMSetRenderTargets(1, &_render_target_view, nullptr);
+                context->OMSetRenderTargets(1, _render_target_view.GetAddressOf(), nullptr);
                 auto texture = _image->texture();
                 if (texture.view)
                 {
