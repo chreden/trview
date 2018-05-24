1b4c4c491270b16d90bfb443adc29e0640d68571
Chris Eden <4263940+chreden@users.noreply.github.com>
2018-05-24 22:24:32 +0100

    Add test project for events
    Empty project at the moment

---
+++ trview.common.tests/EventTests.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+#include <trview.common/Timer.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace trview
+{
+    TEST_CLASS(EventTests)
+    {
+        TEST_METHOD(Constructor)
+        {
+        }
+    };
+}
