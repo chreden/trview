903e914a5bff5e68990a3766cb3a1833fc8e21ad
Chris Eden <4263940+chreden@users.noreply.github.com>
2018-05-28 15:57:30 +0100

    Add input tests project
    Empty mouse and keyboard test classes

---
+++ trview.input.tests/KeyboardTests.cpp
@@ -0,0 +1,14 @@
+#include "CppUnitTest.h"
+
+#include <trview.input/Keyboard.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace trview
+{
+    TEST_CLASS(KeyboardTests)
+    {
+    public:
+                
+    };
+}
---
+++ trview.input.tests/MouseTests.cpp
@@ -0,0 +1,14 @@
+#include "CppUnitTest.h"
+
+#include <trview.input/Mouse.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace trview
+{
+    TEST_CLASS(MouseTests)
+    {
+    public:
+
+    };
+}
