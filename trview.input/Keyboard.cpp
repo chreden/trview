20f189f2a2d55207fe7415c65b5d5a145c08457e
Chris Eden <4263940+chreden@users.noreply.github.com>
2018-05-28 20:18:04 +0100

    Add comments, unregister keyboards at destruction
    Remove keyboards from the keyboard vector once they are destroyed.

--- trview.input/Keyboard.cpp
+++ trview.input/Keyboard.cpp
@@ -11,6 +11,7 @@ namespace trview
         namespace
         {
             WNDPROC old_procedure;
+
             std::unordered_map<HWND, std::vector<Keyboard*>> all_keyboards;
 
             LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -20,24 +21,23 @@ namespace trview
                 if (iter != all_keyboards.end())
                 {
                     const auto& kbs = iter->second;
-
                     switch (message)
                     {
-                    case WM_KEYDOWN:
-                    {
-                        std::for_each(kbs.begin(), kbs.end(), [wParam](auto& k) { k->on_key_down(static_cast<uint16_t>(wParam)); });
-                        break;
-                    }
-                    case WM_CHAR:
-                    {
-                        std::for_each(kbs.begin(), kbs.end(), [wParam](auto& k) { k->on_char(static_cast<uint16_t>(wParam)); });
-                        break;
-                    }
-                    case WM_KEYUP:
-                    {
-                        std::for_each(kbs.begin(), kbs.end(), [wParam](auto& k) { k->on_key_up(static_cast<uint16_t>(wParam)); });
-                        break;
-                    }
+                        case WM_KEYDOWN:
+                        {
+                            std::for_each(kbs.begin(), kbs.end(), [wParam](auto& k) { k->on_key_down(static_cast<uint16_t>(wParam)); });
+                            break;
+                        }
+                        case WM_CHAR:
+                        {
+                            std::for_each(kbs.begin(), kbs.end(), [wParam](auto& k) { k->on_char(static_cast<uint16_t>(wParam)); });
+                            break;
+                        }
+                        case WM_KEYUP:
+                        {
+                            std::for_each(kbs.begin(), kbs.end(), [wParam](auto& k) { k->on_key_up(static_cast<uint16_t>(wParam)); });
+                            break;
+                        }
                     }
                 }
 
@@ -45,12 +45,31 @@ namespace trview
             }
         }
 
+        // Subclass the window if it hasn't already been done. Allows for us to get the window
+        // messages and look for ones related to keyboard input. The messages are then passed
+        // on to the original window procedure.
+        void subclass_window(HWND window)
+        {
+            if (old_procedure == nullptr)
+            {
+                old_procedure = (WNDPROC)GetWindowLongPtr(window, GWLP_WNDPROC);
+                SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
+            }
+        }
+
         Keyboard::Keyboard(HWND window)
             : _window(window)
         {
-            old_procedure = (WNDPROC)GetWindowLongPtr(window, GWLP_WNDPROC);
-            SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
+            subclass_window(window);
             all_keyboards[window].push_back(this);
+        }
+
+        Keyboard::~Keyboard()
+        {
+            // Remove the keyboard from the collection so the window procedure doesn't try
+            // to keep sending messages to it after it has been destroyed.
+            auto& kbs = all_keyboards[_window];
+            kbs.erase(std::remove(kbs.begin(), kbs.end(), this), kbs.end());
         }
 
         bool Keyboard::control() const
--- trview.input/Keyboard.h
+++ trview.input/Keyboard.h
@@ -1,3 +1,9 @@
+/// @file Keyboard.h
+/// @brief Class that receives keyboard input on a specific window.
+/// 
+/// Given a window will receive input messages and translate them into events that
+/// can be subscribed to.
+
 #pragma once
 
 #include <cstdint>
@@ -10,14 +16,30 @@ namespace trview
 {
     namespace input
     {
+        /// Receives keyboard input for a specific window.
         class Keyboard
         {
         public:
+            /// Create a keyboard to listen to keyboard messages for a specific window.
+            /// @param window The window to listen to.
             explicit Keyboard(HWND window);
 
+            ~Keyboard();
+
+            /// Determines whether the control key is currently pressed.
+            /// @returns Whether control is currently pressed.
             bool control() const;
+
+            /// Event raised when the user presses a key. The key is passed as a parameter
+            /// to the event listeners.
             Event<uint16_t> on_key_down;
+
+            /// Event raised when the user releases a key. The key is passed as a parameter
+            /// to the event listeners.
             Event<uint16_t> on_key_up;
+
+            /// Event raised when the user enters a text character. The character is passed as
+            /// a parameter to the event listener.
             Event<uint16_t> on_char;
         private:
             HWND _window;
