590cb1318f9becb344f6bbb7c461a635a38dff69
Chris Eden <4263940+chreden@users.noreply.github.com>
2018-05-29 22:40:11 +0100

    Add mouse up tests
    Make all others fail

--- trview.input.tests/MouseTests.cpp
+++ trview.input.tests/MouseTests.cpp
@@ -62,22 +62,43 @@ namespace trview
 
             TEST_METHOD(MouseUp)
             {
+                int times_called = 0;
+                Mouse::Button button_received = Mouse::Button::Left;
 
+                HWND window = create_test_window();
+                Mouse mouse(window);
+
+                mouse.mouse_up +=
+                    [&times_called, &button_received](auto button)
+                {
+                    ++times_called;
+                    button_received = button;
+                };
+
+                RAWINPUT input;
+                memset(&input, 0, sizeof(input));
+                input.header.dwType = RIM_TYPEMOUSE;
+                input.header.dwSize = sizeof(RAWINPUT);
+                input.data.mouse.usButtonFlags |= RI_MOUSE_RIGHT_BUTTON_UP;
+                mouse.process_input(input);
+
+                Assert::AreEqual(1, times_called);
+                Assert::AreEqual(Mouse::Button::Right, button_received);
             }
 
             TEST_METHOD(MouseWheel)
             {
-
+                Assert::Fail();
             }
 
             TEST_METHOD(MouseMove)
             {
-
+                Assert::Fail();
             }
 
             TEST_METHOD(MousePosition)
             {
-
+                Assert::Fail();
             }
         };
     }
