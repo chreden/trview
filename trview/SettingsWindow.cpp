5f543a247bd6aa77cf5b8487c2afcc6bf1f2181d
chreden <4263940+chreden@users.noreply.github.com>
2018-06-11 12:37:11 +0100

    Start on the settings window

--- trview/SettingsWindow.cpp
+++ trview/SettingsWindow.cpp
@@ -2,7 +2,8 @@
 #include "SettingsWindow.h"
 
 #include <trview.ui/Control.h>
-#include <trview.ui/Window.h>
+#include <trview.ui/StackPanel.h>
+#include <trview.ui/GroupBox.h>
 
 using namespace trview::ui;
 
@@ -10,7 +11,19 @@ namespace trview
 {
     SettingsWindow::SettingsWindow(Control& parent)
     {
-        auto window = std::make_unique<Window>(Point(400, 200), Size(200, 100), Colour(1.0f, 0.2f, 0.2f, 0.2f));
+        const auto background_colour = Colour(1.0f, 0.4f, 0.4f, 0.4f);
+
+        auto window = std::make_unique<StackPanel>(Point(400, 200), Size(200, 100), background_colour, Size(5,5));
+
+        // Things to add:
+        //      Window title
+        //      Close button
+        //      Checkboxes
+
+        auto group = std::make_unique<GroupBox>(Point(), Size(190, 90), background_colour, Colour(1.0f, 0.0f, 0.0f, 0.0f), L"Settings");
+
+        window->add_child(std::move(group));
+
         parent.add_child(std::move(window));
     }
 }
