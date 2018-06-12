ce4e127ef61aae5ddfa9b322b061ce6621ce6173
Chris Eden <4263940+chreden@users.noreply.github.com>
2018-06-12 22:06:24 +0100

    Add a spacer to the settings window

--- trview/SettingsWindow.cpp
+++ trview/SettingsWindow.cpp
@@ -17,12 +17,7 @@ namespace trview
     {
         const auto background_colour = Colour(1.0f, 0.5f, 0.5f, 0.5f);
 
-        auto window = std::make_unique<StackPanel>(Point(400, 200), Size(400, 300), background_colour, Size(5,5));
-
-        // Things to add:
-        //      Window title
-        //      Close button
-        //      Checkboxes
+        auto window = std::make_unique<StackPanel>(Point(400, 200), Size(400, 300), background_colour, Size(5,0));
 
         auto group = std::make_unique<GroupBox>(Point(), Size(390, 290), background_colour, Colour(1.0f, 0.0f, 0.0f, 0.0f), L"Settings");
         auto panel = std::make_unique<StackPanel>(Point(12, 20), Size(366, 250), background_colour, Size());
@@ -30,8 +25,13 @@ namespace trview
         const auto check_off = texture_storage.lookup("check_off");
         const auto check_on = texture_storage.lookup("check_on");
 
-        auto vsync = std::make_unique<Checkbox>(Point(12, 20), Size(16, 16), check_off, check_on, L"Vsync");
+        auto vsync = std::make_unique<Checkbox>(Point(), Size(16, 16), check_off, check_on, L"Vsync");
         panel->add_child(std::move(vsync));
+
+        panel->add_child(std::make_unique<Window>(Point(), Size(1, 5), background_colour));
+
+        auto go_to_lara = std::make_unique<Checkbox>(Point(), Size(16, 16), check_off, check_on, L"Go to Lara");
+        panel->add_child(std::move(go_to_lara));
 
         group->add_child(std::move(panel));
         window->add_child(std::move(group));
