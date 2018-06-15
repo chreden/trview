b92be3d8c517caf536dc0c148ba17680094f5019
chreden <4263940+chreden@users.noreply.github.com>
2018-06-15 13:02:54 +0100

    Comments for SizeDimension

--- trview.ui/SizeDimension.h
+++ trview.ui/SizeDimension.h
@@ -1,13 +1,20 @@
+/// @file SizeDimension.h
+/// @brief Describes how a control should resize.
+
 #pragma once
 
 namespace trview
 {
     namespace ui
     {
+        /// Describes how a control should resize.
         enum class SizeDimension
         {
+            /// The control should resize its width only.
             Width,
+            /// The control should resize its height only.
             Height,
+            /// The control should resize its width and height.
             All
         };
     }
