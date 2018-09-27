1c43dcc5e38b6e804c74ac108c9b3689678cee9c
chreden <4263940+chreden@users.noreply.github.com>
2018-09-27 07:25:47 +0100

    Change to double thickness yellow

--- trview.shaders/selection_pixel_shader.hlsl
+++ trview.shaders/selection_pixel_shader.hlsl
@@ -17,7 +17,7 @@ SamplerState samplerState;
 float4 main(PixelInput input) : SV_TARGET
 {
     // If this is a red pixel and there are surrounding pixels that are black, make it a white pixel.
-    float2x4 samples = 
+    float2x4 inner_samples =
     {
         1 - tex.Sample(samplerState, input.uv + float2(width_pixel, 0)).r,
         1 - tex.Sample(samplerState, input.uv + float2(width_pixel, height_pixel)).r,
@@ -29,6 +29,33 @@ float4 main(PixelInput input) : SV_TARGET
         1 - tex.Sample(samplerState, input.uv + float2(width_pixel, -height_pixel)).r
     };
 
-    clip(-1.5 + tex.Sample(samplerState, input.uv).r + any(samples));
-    return float4(1, 0, 1, 1);
+    float4x4 outer_samples = 
+    {
+        // Top row
+        1 - tex.Sample(samplerState, input.uv + float2(-2 * width_pixel, -2 * height_pixel)).r,
+        1 - tex.Sample(samplerState, input.uv + float2(-width_pixel, -2 * height_pixel)).r,
+        1 - tex.Sample(samplerState, input.uv + float2(0, -2 * height_pixel)).r,
+        1 - tex.Sample(samplerState, input.uv + float2(width_pixel, -2 * height_pixel)).r,
+        1 - tex.Sample(samplerState, input.uv + float2(2 * width_pixel, -2 * height_pixel)).r,
+
+        // Right column
+        1 - tex.Sample(samplerState, input.uv + float2(2 * width_pixel, -height_pixel)).r,
+        1 - tex.Sample(samplerState, input.uv + float2(2 * width_pixel, 0)).r,
+        1 - tex.Sample(samplerState, input.uv + float2(2 * width_pixel, height_pixel)).r,
+        1 - tex.Sample(samplerState, input.uv + float2(2 * width_pixel, 2 * height_pixel)).r,
+
+        // Bottom row
+        1 - tex.Sample(samplerState, input.uv + float2(width_pixel, 2 * height_pixel)).r,
+        1 - tex.Sample(samplerState, input.uv + float2(0, 2 * height_pixel)).r,
+        1 - tex.Sample(samplerState, input.uv + float2(-width_pixel, 2 * height_pixel)).r,
+        1 - tex.Sample(samplerState, input.uv + float2(-2 * width_pixel, 2 * height_pixel)).r,
+
+        // Left row
+        1 - tex.Sample(samplerState, input.uv + float2(-2 * width_pixel, -height_pixel)).r,
+        1 - tex.Sample(samplerState, input.uv + float2(-2 * width_pixel, 0)).r,
+        1 - tex.Sample(samplerState, input.uv + float2(-2 * width_pixel, height_pixel)).r,
+    };
+
+    clip(-1.5 + tex.Sample(samplerState, input.uv).r + min(1, any(inner_samples) + any(outer_samples)));
+    return float4(1, 1, 0, 1);
 }
