c9ddc4acf2166627f78856da6f9b2ad66669577c
chreden <4263940+chreden@users.noreply.github.com>
2018-10-03 21:22:18 +0100

    Add the trigger window manager
    This is a clone of the item window manager

---
+++ trview.app/TriggersWindowManager.cpp
@@ -0,0 +1,55 @@
+#include "TriggersWindowManager.h"
+#include "WindowIDs.h"
+#include <trview.ui.render/Renderer.h>
+#include <trview.graphics/DeviceWindow.h>
+
+namespace trview
+{
+    TriggersWindowManager::TriggersWindowManager(graphics::Device& device, graphics::IShaderStorage& shader_storage, graphics::FontFactory& font_factory, HWND window)
+        : _device(device), _shader_storage(shader_storage), _font_factory(font_factory), MessageHandler(window)
+    {
+    }
+
+    void TriggersWindowManager::process_message(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
+    {
+        if (message == WM_COMMAND &&
+            LOWORD(wParam) == ID_APP_WINDOWS_ITEMS ||
+            LOWORD(wParam) == ID_APP_ACCEL_ITEM_WINDOW)
+        {
+            create_window();
+        }
+    }
+
+    void TriggersWindowManager::render(graphics::Device& device, bool vsync)
+    {
+        if (!_closing_windows.empty())
+        {
+            _windows.erase(std::remove_if(_windows.begin(), _windows.end(),
+                [&](auto& window) { return std::find(_closing_windows.begin(), _closing_windows.end(), window.get()) != _closing_windows.end(); }), _windows.end());
+            _closing_windows.clear();
+        }
+
+        for (auto& window : _windows)
+        {
+            window->render(device, vsync);
+        }
+    }
+
+    void TriggersWindowManager::create_window()
+    {
+        auto items_window = std::make_unique<TriggersWindow>(_device, _shader_storage, _font_factory, window());
+        items_window->on_item_selected += on_item_selected;
+        items_window->on_trigger_selected += on_trigger_selected;
+        // items_window->set_items(_items);
+        items_window->set_triggers(_triggers);
+        items_window->set_current_room(_current_room);
+
+        const auto window = items_window.get();
+        _token_store.add(items_window->on_window_closed += [window, this]()
+        {
+            _closing_windows.push_back(window);
+        });
+
+        _windows.push_back(std::move(items_window));
+    }
+}
---
+++ trview.app/TriggersWindowManager.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include <trview.common/MessageHandler.h>
+#include <trview.graphics/Device.h>
+#include <trview.graphics/IShaderStorage.h>
+#include <trview.graphics/FontFactory.h>
+#include <trview.common/TokenStore.h>
+#include "TriggersWindow.h"
+
+namespace trview
+{
+    /// Controls and creates TriggersWindows.
+    class TriggersWindowManager final : public MessageHandler
+    {
+    public:
+        /// Create an TriggersWindowManager.
+        /// @param device The device to use for triggers windows.
+        /// @param shader_storage The shader storage for triggers windows.
+        /// @param font_factory The font_factory for triggers windows.
+        /// @param window The parent window of the triggers window.
+        explicit TriggersWindowManager(graphics::Device& device, graphics::IShaderStorage& shader_storage, graphics::FontFactory& font_factory, HWND window);
+
+        /// Destructor for the TriggersWindowManager.
+        virtual ~TriggersWindowManager() = default;
+
+        /// Handles a window message.
+        /// @param window The window that received the message.
+        /// @param message The message that was received.
+        /// @param wParam The WPARAM for the message.
+        /// @param lParam The LPARAM for the message.
+        virtual void process_message(HWND window, UINT message, WPARAM wParam, LPARAM lParam) override;
+
+        /// Event raised when an item is selected in one of the trigger windows.
+        Event<Item> on_item_selected;
+
+        /// Event raised when a trigger is selected in one of the trigger windows.
+        Event<Trigger> on_trigger_selected;
+
+        /// Render all of the triggers windows.
+        /// @param device The device to use to render.
+        /// @param vsync Whether to use vsync.
+        void render(graphics::Device& device, bool vsync);
+
+        /// Create a new triggers window.
+        void create_window();
+    private:
+        std::vector<std::unique_ptr<TriggersWindow>> _windows;
+        std::vector<TriggersWindow*> _closing_windows;
+        std::vector<Item> _items;
+        std::vector<Trigger> _triggers;
+        graphics::Device& _device;
+        graphics::IShaderStorage& _shader_storage;
+        graphics::FontFactory& _font_factory;
+        uint32_t _current_room{ 0u };
+        TokenStore _token_store;
+    };
+}
+
