d1f9490df4044fb05309e56679aa71f5f3c5fee4
chreden <4263940+chreden@users.noreply.github.com>
2018-12-30 10:06:25 +0000

    Create waypoint class
    Not used yet

---
+++ trview.app/Waypoint.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <SimpleMath.h>
+
+namespace trview
+{
+    class Waypoint final
+    {
+    public:
+        
+    private:
+        std::string                  _name;
+        DirectX::SimpleMath::Vector3 _position;
+    };
+}
