633634392e238ce1c6e55257dbf746133282f49b
chreden <4263940+chreden@users.noreply.github.com>
2019-01-12 23:03:14 +0000

    Add colours constants for context menu
    Make it slightly more readable.

--- trview.app/ContextMenu.cpp
+++ trview.app/ContextMenu.cpp
@@ -6,20 +6,28 @@ using namespace trview::ui;
 
 namespace trview
 {
+    namespace Colours
+    {
+        const Colour Background{ 0.15f, 0.15f, 0.15f };
+        const Colour Button{ 0.2f, 0.2f, 0.2f };
+        const Colour Enabled{ 1.0f, 1.0f, 1.0f };
+        const Colour Disabled{ 0.4f, 0.4f, 0.4f };
+    }
+
     ContextMenu::ContextMenu(Control& parent)
     {
-        _menu = parent.add_child(std::make_unique<StackPanel>(Point(300, 300), Size(200, 100), Colour(0.15f, 0.15f, 0.15f), Size(1,1)));
+        _menu = parent.add_child(std::make_unique<StackPanel>(Point(300, 300), Size(200, 100), Colours::Background, Size(1, 1)));
 
         // Add a button in a little area with some writing.
-        auto button_area = std::make_unique<StackPanel>(Point(), Size(100, 20), Colour(0.2f, 0.2f, 0.2f), Size(), StackPanel::Direction::Horizontal);
+        auto button_area = std::make_unique<StackPanel>(Point(), Size(100, 20), Colours::Button, Size(), StackPanel::Direction::Horizontal);
         auto button = std::make_unique<Button>(Point(), Size(24, 24), L"+");
         button->on_click += on_add_waypoint;
         button_area->add_child(std::move(button));
-        button_area->add_child(std::make_unique<Label>(Point(), Size(100, 24), Colour(0.2f, 0.2f, 0.2f), L"Add Waypoint", 8, graphics::TextAlignment::Centre, graphics::ParagraphAlignment::Centre));
+        button_area->add_child(std::make_unique<Label>(Point(), Size(100, 24), Colours::Button, L"Add Waypoint", 8, graphics::TextAlignment::Centre, graphics::ParagraphAlignment::Centre));
         _menu->add_child(std::move(button_area));
 
         // Add the similar remove waypoint button 
-        auto remove_button_area = std::make_unique<StackPanel>(Point(), Size(100, 20), Colour(0.2f, 0.2f, 0.2f), Size(), StackPanel::Direction::Horizontal);
+        auto remove_button_area = std::make_unique<StackPanel>(Point(), Size(100, 20), Colours::Button, Size(), StackPanel::Direction::Horizontal);
         auto remove_button = std::make_unique<Button>(Point(), Size(24, 24), L"-");
         _token_store.add(remove_button->on_click += [&]() 
         {
@@ -29,7 +37,7 @@ namespace trview
             }
         });
         remove_button_area->add_child(std::move(remove_button));
-        auto remove_label = std::make_unique<Label>(Point(), Size(100, 24), Colour(0.2f, 0.2f, 0.2f), L"Remove Waypoint", 8, graphics::TextAlignment::Centre, graphics::ParagraphAlignment::Centre);
+        auto remove_label = std::make_unique<Label>(Point(), Size(100, 24), Colours::Button , L"Remove Waypoint", 8, graphics::TextAlignment::Centre, graphics::ParagraphAlignment::Centre);
         _remove_label = remove_button_area->add_child(std::move(remove_label));
         _menu->add_child(std::move(remove_button_area));
 
@@ -49,6 +57,6 @@ namespace trview
     void ContextMenu::set_remove_enabled(bool value)
     {
         _remove_enabled = value;
-        _remove_label->set_text_colour(value ? Colour(1.0f, 1.0f, 1.0f) : Colour(0.4f, 0.4f, 0.4f));
+        _remove_label->set_text_colour(value ? Colours::Enabled : Colours::Disabled);
     }
 }
