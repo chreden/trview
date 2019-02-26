8b8af6636bb0edcc75e94aeaff1ddbf2074298b4
chreden <4263940+chreden@users.noreply.github.com>
2019-02-26 23:05:29 +0000

    Update the settings window
    Use the transparent scheme

--- trview/LevelInfo.cpp
+++ trview/LevelInfo.cpp
@@ -33,17 +33,16 @@ namespace trview
         }
 
         using namespace ui;
-        auto panel = std::make_unique<StackPanel>(Point(control.size().width / 2.0f - 50, 0), Size(100, 24), Colour(1.0f, 0.5f, 0.5f, 0.5f), Size(5, 5), StackPanel::Direction::Horizontal, SizeMode::Auto);
+        auto panel = std::make_unique<StackPanel>(Point(control.size().width / 2.0f - 50, 0), Size(100, 24), Colour(0.5f, 0.0f, 0.0f, 0.0f), Size(5, 5), StackPanel::Direction::Horizontal, SizeMode::Auto);
         panel->set_margin(Size(5, 5));
         auto version = std::make_unique<Image>(Point(), Size(16, 16));
         version->set_background_colour(Colour(0, 0, 0, 0));
         version->set_texture(get_version_image(trlevel::LevelVersion::Unknown));
 
-        auto name = std::make_unique<Label>(Point(), Size(74, 16), Colour(1.0f, 0.5f, 0.5f, 0.5f), L"No level", 8, graphics::TextAlignment::Centre, graphics::ParagraphAlignment::Centre, SizeMode::Auto);
+        auto name = std::make_unique<Label>(Point(), Size(74, 16), Colour::Transparent, L"No level", 8, graphics::TextAlignment::Centre, graphics::ParagraphAlignment::Centre, SizeMode::Auto);
         name->set_vertical_alignment(Align::Centre);
 
         auto settings = std::make_unique<Button>(Point(), Size(16, 16), texture_storage.lookup("settings"), texture_storage.lookup("settings"));
-        settings->set_background_colour(Colour(0.4f, 0.4f, 0.4f));
         settings->on_click += on_toggle_settings;
 
         _version = panel->add_child(std::move(version));
