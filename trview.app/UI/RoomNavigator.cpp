42aaf2dcdfbab2f72144b979ae556ef5ad0e915c
chreden <4263940+chreden@users.noreply.github.com>
2019-03-18 20:40:27 +0000

    Add current/max back to room navigator

--- trview.app/UI/RoomNavigator.cpp
+++ trview.app/UI/RoomNavigator.cpp
@@ -16,9 +16,17 @@ namespace trview
     {
         using namespace ui;
 
-        auto rooms_groups = std::make_unique<GroupBox>(Point(), Size(150, 110), Colour::Transparent, Colour::Grey, L"Room");
+        auto rooms_groups = std::make_unique<GroupBox>(Point(), Size(150, 90), Colour::Transparent, Colour::Grey, L"Room");
 
-        auto listbox = std::make_unique<Listbox>(Point(4, 12), Size(140, 80), Colour::Transparent);
+        auto room_controls = std::make_unique<StackPanel>(Point(4, 12), Size(140, 30), Colour::Transparent, Size(), StackPanel::Direction::Horizontal);
+        _current = room_controls->add_child(std::make_unique<NumericUpDown>(Point(), Size(50, 20), Colour::Transparent, texture_storage.lookup("numeric_up"), texture_storage.lookup("numeric_down"), 0, 0));
+        _current->on_value_changed += on_room_selected;
+
+        room_controls->add_child(std::make_unique<Label>(Point(), Size(40, 20), Colour::Transparent, L"of", 8, graphics::TextAlignment::Centre, graphics::ParagraphAlignment::Centre));
+        _max = room_controls->add_child(std::make_unique<Label>(Point(), Size(50, 20), Colour::Transparent, L"0", 8, graphics::TextAlignment::Centre, graphics::ParagraphAlignment::Centre));
+        rooms_groups->add_child(std::move(room_controls));
+
+        auto listbox = std::make_unique<Listbox>(Point(4, 36), Size(140, 80), Colour::Transparent);
         listbox->set_show_scrollbar(false);
         listbox->set_show_headers(false);
         listbox->set_columns(
@@ -27,24 +35,6 @@ namespace trview
                 { Listbox::Column::Type::String, L"Value", 90 }
             });
         _listbox = rooms_groups->add_child(std::move(listbox));
-
-        /*
-        auto room_controls = std::make_unique<StackPanel>(Point(22, 17), Size(96, 60), Colour::Transparent);
-        auto room_number_labels = std::make_unique<StackPanel>(Point(), Size(96, 30), Colour::Transparent, Size(), StackPanel::Direction::Horizontal);
-        auto room_number = std::make_unique<NumericUpDown>(Point(), Size(40, 20), Colour::Transparent, texture_storage.lookup("numeric_up"), texture_storage.lookup("numeric_down"), 0, 0);
-        auto room_number_label = std::make_unique<Label>(Point(),Size(8, 16), Colour::Transparent, L" / ", 8, graphics::TextAlignment::Left, graphics::ParagraphAlignment::Centre, SizeMode::Auto);
-        auto room_max_label = std::make_unique<Label>(Point(), Size(40, 20), Colour::Transparent, L"0", 8, graphics::TextAlignment::Centre, graphics::ParagraphAlignment::Centre);
-
-        room_number->on_value_changed += on_room_selected;
-
-        _current = room_number_labels->add_child(std::move(room_number));
-        room_number_labels->add_child(std::move(room_number_label));
-        _max = room_number_labels->add_child(std::move(room_max_label));
-
-        room_controls->add_child(std::move(room_number_labels));
-
-        rooms_groups->add_child(std::move(room_controls));
-        */
         parent.add_child(std::move(rooms_groups));
     }
 
@@ -63,12 +53,12 @@ namespace trview
     void RoomNavigator::set_max_rooms(uint32_t max_rooms)
     {
         uint32_t adjusted_size = max_rooms ? max_rooms - 1 : 0u;
-        // _current->set_maximum(adjusted_size);
-        // _max->set_text(std::to_wstring(adjusted_size));
+        _current->set_maximum(adjusted_size);
+        _max->set_text(std::to_wstring(adjusted_size));
     }
 
     void RoomNavigator::set_selected_room(uint32_t selected_room)
     {
-        // _current->set_value(selected_room);
+        _current->set_value(selected_room);
     }
 }
