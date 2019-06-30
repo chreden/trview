8906cb1c4f79d27f64a71e83b483c2afdcdb9251
chreden <4263940+chreden@users.noreply.github.com>
2019-06-30 23:33:45 +0100

    Comments on IWindowTester interface

--- trview.input/IWindowTester.h
+++ trview.input/IWindowTester.h
@@ -6,11 +6,20 @@ namespace trview
 {
     namespace input
     {
+        /// Used to check for Windows information.
         struct IWindowTester
         {
             virtual ~IWindowTester() = 0;
+
+            /// Gets the window that is currently under the cursor.
             virtual Window window_under_cursor() const = 0;
+
+            /// Gets the screen width - or the virtual screen width if on remote desktop.
+            /// @param rdp Whether to check the remote screen size.
             virtual int screen_width(bool rdp) const = 0;
+
+            /// Gets the screen height - or the virtual screen height if on remote desktop.
+            /// @param rdp Whether to check the remote screen size.
             virtual int screen_height(bool rdp) const = 0;
         };
     }
