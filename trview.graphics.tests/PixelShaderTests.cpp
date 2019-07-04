1663e34fee505d1a0c674a8fde188c6f1da9af30
chreden <4263940+chreden@users.noreply.github.com>
2019-07-04 23:32:12 +0100

    Convert graphics tests

--- trview.graphics.tests/PixelShaderTests.cpp
+++ trview.graphics.tests/PixelShaderTests.cpp
@@ -1,24 +1,10 @@
-#include "CppUnitTest.h"
-
+#include "gtest/gtest.h"
 #include <trview.graphics/PixelShader.h>
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace trview::graphics;
 
-namespace trview
+/// Tests that trying to create a pixel shader with no data throws.
+TEST(PixelShader, EmptyDataThrows)
 {
-    namespace graphics
-    {
-        namespace tests
-        {
-            TEST_CLASS(PixelShaderTests)
-            {
-                // Tests that trying to create a pixel shader with no data throws.
-                TEST_METHOD(EmptyData)
-                {
-                    graphics::Device device;
-                    Assert::ExpectException<std::exception>([&device]() { PixelShader{ device, std::vector<uint8_t>() }; });
-                }
-            };
-        }
-    }
-}
+    ASSERT_THROW(PixelShader(Device(), std::vector<uint8_t>()), std::exception);
+};
--- trview.graphics.tests/ShaderStorageTests.cpp
+++ trview.graphics.tests/ShaderStorageTests.cpp
@@ -1,47 +1,29 @@
-#include "CppUnitTest.h"
-
+#include "gtest/gtest.h"
 #include <trview.graphics/ShaderStorage.h>
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace trview::graphics;
 
-namespace trview
+class TestShader final : public IShader
 {
-    namespace graphics
-    {
-        namespace tests
-        {
-            TEST_CLASS(ShaderStorageTests)
-            {
-            private:
-                class TestShader final : public IShader
-                {
-                public:
-                    virtual ~TestShader() = default;
+public:
+    virtual ~TestShader() = default;
+    virtual void apply(const Microsoft::WRL::ComPtr<ID3D11DeviceContext>&) override {}
+};
 
-                    virtual void apply(const Microsoft::WRL::ComPtr<ID3D11DeviceContext>&) override
-                    {
-                    }
-                };
+/// Test that the shader is added to the store and can then be retrieved.
+TEST(ShaderStorage, AddAndRetrieveShader)
+{
+    ShaderStorage storage;
+    ASSERT_EQ(nullptr, storage.get("test"));
+    storage.add("test", std::make_unique<TestShader>());
+    ASSERT_NE(nullptr, storage.get("test"));
+}
 
-            public:
-                // Test that the shader is added to the store and can then be retrieved.
-                TEST_METHOD(AddAndRetrieveShader)
-                {
-                    ShaderStorage storage;
-                    Assert::IsNull(storage.get("test"));
-                    storage.add("test", std::make_unique<TestShader>());
-                    Assert::IsNotNull(storage.get("test"));
-                }
-
-                // Test that a null shader is rejected by the store.
-                TEST_METHOD(NullShader)
-                {
-                    ShaderStorage storage;
-                    Assert::IsNull(storage.get("test"));
-                    Assert::ExpectException<std::exception>([&storage]() { storage.add("test", nullptr); });
-                    Assert::IsNull(storage.get("test"));
-                }
-            };
-        }
-    }
+/// Test that a null shader is rejected by the store.
+TEST(ShaderStorage, NullShader)
+{
+    ShaderStorage storage;
+    ASSERT_EQ(nullptr, storage.get("test"));
+    ASSERT_THROW(storage.add("test", nullptr), std::exception);
+    ASSERT_EQ(nullptr, storage.get("test"));
 }
