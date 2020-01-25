691558426be54b7a6b15a0d28c657f3851822a9a
chreden <4263940+chreden@users.noreply.github.com>
2020-01-25 16:40:32 +0000

    Move action node rendering into a class

---
+++ trview.app/Routing/ActionMenu.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <trview.graphics/Device.h>
+#include "ActionNodeOption.h"
+
+namespace trview
+{
+    struct ICamera;
+    class TransparencyBuffer;
+
+    class ActionMenu
+    {
+    public:
+        ActionMenu();
+
+        void render(const graphics::Device& device,
+            const ICamera& camera,
+            TransparencyBuffer& transparency_buffer,
+            const DirectX::SimpleMath::Vector3& previous_waypoint,
+            const DirectX::SimpleMath::Vector3& next_waypoint,
+            bool selected);
+    private:
+        void generate_geometry();
+
+        std::unique_ptr<ActionNodeOption> _action;
+    };
+}
--- trview.app/Routing/Route.cpp
+++ trview.app/Routing/Route.cpp
@@ -56,34 +56,6 @@ namespace trview
     Route::Route(const graphics::Device& device, const graphics::IShaderStorage& shader_storage)
         : _waypoint_mesh(create_cube_mesh(device)), _selection_renderer(device, shader_storage), _transparency_buffer(device)
     {
-        // Load the sprite mesh (temporarily here)
-        // Get the first sprite image.
-        // auto sprite = level.get_sprite_texture(sprite_sequence.Offset);
-        // auto texture = texture_storage.texture(sprite.Tile);
-
-        // Calculate UVs.
-        float u = 0.0f;
-        float v = 0.0f;
-        float width = 1.0f;
-        float height = 1.0f;
-
-        // Generate quad.
-        using namespace DirectX::SimpleMath;
-        std::vector<MeshVertex> vertices
-        {
-            { Vector3(-0.5f, -0.5f, 0), Vector3::Zero, Vector2(u, v + height), Color(1,1,1,1) },
-            { Vector3(0.5f, -0.5f, 0), Vector3::Zero, Vector2(u + width, v + height), Color(1,1,1,1) },
-            { Vector3(-0.5f, 0.5f, 0), Vector3::Zero, Vector2(u, v), Color(1,1,1,1) },
-            { Vector3(0.5f, 0.5f, 0), Vector3::Zero, Vector2(u + width, v), Color(1,1,1,1) },
-        };
-
-        std::vector<TransparentTriangle> transparent_triangles
-        {
-            { vertices[0].pos, vertices[1].pos, vertices[2].pos, vertices[0].uv, vertices[1].uv, vertices[2].uv, TransparentTriangle::Untextured, TransparentTriangle::Mode::Normal },
-            { vertices[2].pos, vertices[1].pos, vertices[3].pos, vertices[2].uv, vertices[1].uv, vertices[3].uv, TransparentTriangle::Untextured, TransparentTriangle::Mode::Normal },
-        };
-
-        _selected_action = std::make_unique<ActionNodeOption>(transparent_triangles);
     }
 
     void Route::add(const Vector3& position, uint32_t room)
@@ -208,43 +180,7 @@ namespace trview
                 const auto length = (next_waypoint - current).Length();
                 const auto to_wvp = Matrix::CreateScale(RopeThickness, RopeThickness, length) * matrix * camera.view_projection();
                 _waypoint_mesh->render(device.context(), to_wvp, texture_storage, _colour);
-
-                // Render the action sprite above the midpoint of the path.
-                // Figure out which way the route is going....
-                auto to = next_waypoint - current;
-                to.Normalize();
-                to *= 0.1f;
-
-                Vector3 first = XMVector3Project(current, 0, 0, camera.view_size().width, camera.view_size().height, 0, 1.0f, camera.projection(), camera.view(), Matrix::Identity);
-                Vector3 second = XMVector3Project(current + to, 0, 0, camera.view_size().width, camera.view_size().height, 0, 1.0f, camera.projection(), camera.view(), Matrix::Identity);
-                bool flip = first.x < second.x;
-                float scale = 0.35f;
-
-                Vector3 forward = camera.forward();
-                auto scaling = Matrix::CreateScale(scale * (flip ? -1.0f : 1.0f), scale, scale);
-                auto billboard = Matrix::CreateBillboard(mid, camera.rendering_position(), camera.up(), &forward) * Matrix::CreateTranslation(0, -0.1f, 0);
-                auto world = scaling * billboard;
-                _selected_action->render(world, _transparency_buffer);
-
-                float scale2 = 0.2f;
-                auto scaling2 = Matrix::CreateScale(scale2 * (flip ? -1.0f : 1.0f), scale2, scale2);
-
-                if (_selected_action_index.has_value() && i == _selected_action_index)
-                {
-                    for (int i = 0; i < 8; ++i)
-                    {
-                        // Render another one...
-                        float angle = DirectX::g_XMTwoPi[0] / 8.0f * static_cast<float>(i);
-                        auto rotation = Matrix::CreateRotationZ(angle);
-
-                        auto transform = rotation * camera.view().Invert();
-                        auto offset = Vector3::TransformNormal(Vector3(0, -0.4f, 0), transform);
-
-                        auto billboard2 = Matrix::CreateBillboard(mid + offset, camera.rendering_position(), camera.up(), &forward) * Matrix::CreateTranslation(0, -0.1f, 0);
-                        auto world2 = scaling2 * billboard2;
-                        _selected_action->render(world2, _transparency_buffer);
-                    }
-                }
+                _action_menu.render(device, camera, _transparency_buffer, current, next_waypoint, _selected_action_index == i);
             }
         }
 
