0b85573165ae57ce3017a0e25cd26a8ce427c8bb
chreden <4263940+chreden@users.noreply.github.com>
2020-05-23 21:42:14 +0100

    Adjust camera controls positioning
    Using new groupbox style, don't have to account for the size of the label
    #648

--- trview.app/UI/CameraControls.cpp
+++ trview.app/UI/CameraControls.cpp
@@ -14,21 +14,21 @@ namespace trview
 
         auto camera_window = std::make_unique<GroupBox>(Size(150, 92), Colour::Transparent, Colour::Grey, L"Camera");
 
-        auto reset_camera = std::make_unique<Button>(Point(12, 20), Size(16, 16));
+        auto reset_camera = std::make_unique<Button>(Size(16, 16));
         reset_camera->on_click += on_reset;
 
-        auto reset_camera_label = std::make_unique<Label>(Point(30, 20), Size(40, 16), Colour::Transparent, L"Reset", 8, graphics::TextAlignment::Left, graphics::ParagraphAlignment::Centre);
+        auto reset_camera_label = std::make_unique<Label>(Point(20, 0), Size(40, 16), Colour::Transparent, L"Reset", 8, graphics::TextAlignment::Left, graphics::ParagraphAlignment::Centre);
 
-        auto orbit_camera = std::make_unique<Checkbox>(Point(86, 20), Colour::Transparent, L"Orbit");
+        auto orbit_camera = std::make_unique<Checkbox>(Point(74, 0), Colour::Transparent, L"Orbit");
         _token_store += orbit_camera->on_state_changed += [&](auto) { change_mode(CameraMode::Orbit); };
 
-        auto free_camera = std::make_unique<Checkbox>(Point(12, 42), Colour::Transparent, L"Free");
+        auto free_camera = std::make_unique<Checkbox>(Point(0, 21), Colour::Transparent, L"Free");
         _token_store += free_camera->on_state_changed += [&](auto) { change_mode(CameraMode::Free); };
 
-        auto axis_camera = std::make_unique<Checkbox>(Point(86, 42), Colour::Transparent, L"Axis");
+        auto axis_camera = std::make_unique<Checkbox>(Point(74, 21), Colour::Transparent, L"Axis");
         _token_store += axis_camera->on_state_changed += [&](auto) { change_mode(CameraMode::Axis); };
 
-        auto ortho = std::make_unique<Checkbox>(Point(12, 64), Colour::Transparent, L"Ortho");
+        auto ortho = std::make_unique<Checkbox>(Point(0, 43), Colour::Transparent, L"Ortho");
         _token_store += ortho->on_state_changed += [&](auto ortho_enabled) { change_projection(ortho_enabled ? ProjectionMode::Orthographic : ProjectionMode::Perspective); };
 
         camera_window->add_child(std::move(reset_camera));
