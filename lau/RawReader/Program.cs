e94bba9e5e83a1b8eb7b0c30174b9c82ddfaa7a6
chreden <4263940+chreden@users.noreply.github.com>
2020-09-07 21:03:54 +0100

    Binary templates so far for TRNG levels (#700)
    Add binary template for the DRM files so far.
    Also a program to read the raw images files.
    Documentation will continue during implementation.
    Closes #697, #698

---
+++ lau/RawReader/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawReader
+{
+    class Program
+    {
+        static void ConvertImage(string path)
+        {
+            using var binary = new BinaryReader(new FileStream(path, FileMode.Open));
+
+            // !WAR
+            binary.BaseStream.Seek(4, SeekOrigin.Current);
+            // More unknowns
+            binary.BaseStream.Seek(16, SeekOrigin.Current);
+            var width = binary.ReadInt32();
+            var height = binary.ReadInt32();
+            // ????
+            binary.BaseStream.Seek(100, SeekOrigin.Current);
+
+            using var image = new Bitmap(width, height);
+
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x = 0; x < width; ++x)
+                {
+                    var pixel = binary.ReadInt32();
+                    image.SetPixel(x, y, Color.FromArgb(pixel));
+                }
+            }
+
+            image.Save(Path.Combine("images", Path.GetFileNameWithoutExtension(path) + ".png"), ImageFormat.Png);
+        }
+
+        static void Main(string[] args)
+        {
+            var path = args[0];
+            foreach (var file in Directory.GetFiles(path, "*.raw"))
+            {
+                ConvertImage(file);
+            }
+        }
+    }
+}
