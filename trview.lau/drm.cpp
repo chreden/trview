6f6f5d03f9fbd845149b42feb2a8dc3fd8943be8
chreden <4263940+chreden@users.noreply.github.com>
2020-09-17 15:49:21 +0100

    Implement some of the loading in the drm file
    Also update the binary template somewaht

--- trview.lau/drm.cpp
+++ trview.lau/drm.cpp
@@ -1,5 +1,6 @@
 #include "drm.h"
 #include <fstream>
+#include <unordered_set>
 
 namespace trview
 {
@@ -26,10 +27,10 @@ namespace trview
                 uint32_t separator;
             };
 
-            struct FileHeader
+            struct WorldMeshHeader
             {
-                uint16_t flags[4];
-                uint16_t id;
+                uint32_t type;
+                uint32_t start;
             };
 #pragma pack(pop)
         }
@@ -78,27 +79,58 @@ namespace trview
             uint32_t sections = read<uint32_t>(file);
             std::vector<SectionHeader> headers = read_vector<SectionHeader>(file, sections);
 
-            FileHeader file_header;
-
             uint32_t zero_sections = 0;
+
+            uint32_t section_index = 0;
+
+            std::unordered_map<uint32_t, std::unordered_set<uint32_t>> links;
+
             for (const auto& header : headers)
             {
+                // Read the header for the section if there is one.
+                if (header.preamble > 0)
+                {
+                    auto number_of_headers = (header.preamble / 32) / 8;
+                    for (auto i = 0; i < number_of_headers; ++i)
+                    {
+                        uint32_t referenced_section = read<uint32_t>(file) >> 3;
+                        uint32_t value = read<uint32_t>(file);
+                        links[section_index].insert(referenced_section);
+                    }
+                }
+
                 switch (header.type)
                 {
                     case SectionType::Section:
                     {
                         if (zero_sections == 0)
                         {
-                            skip(file, header.preamble / 32);
-                            file_header = read<FileHeader>(file);
-                            skip(file, header.length - 10);
+                            for (int i = 0; i < 4; ++i)
+                            {
+                                drm->file_header.flags[i] = read<uint16_t>(file);
+                            }
+                            drm->file_header.id = read<uint32_t>(file);
+
+                            if (!drm->file_header.flags[1])
+                            {
+                                // Room:
+                                skip(file, 262);
+                                drm->world_offset = read<Vector3>(file);
+                                skip(file, 6);
+                                skip(file, header.length - 290);
+                            }
+                            else
+                            {
+                                skip(file, 120);
+                                drm->file_header.id_of_next_section = read<uint16_t>(file);
+                                skip(file, 6);
+                            }
                         }
                         else if (zero_sections == 1)
                         {
-                            if (file_header.flags[1])
+                            if (drm->file_header.flags[1])
                             {
                                 // Vertex data - skip for now.
-                                skip(file, header.preamble / 32);
                                 skip(file, header.length);
                             }
                             else
@@ -107,25 +139,78 @@ namespace trview
                                 for (uint32_t i = 0; i < num_vertices; ++i)
                                 {
                                     drm->world_mesh.push_back(read<Vertex>(file));
-                                    skip(file, 14);
                                 }
                             }
                         }
                         else
                         {
-                            skip(file, header.preamble / 32);
-                            skip(file, header.length);
+                            uint32_t type = read<uint32_t>(file);
+                            // Disabled for now.
+                            if (type == -1)
+                            {
+                                file.seekg(-4, std::ios::cur);
+                                // Read world model data:
+                                const uint32_t number_of_headers = (header.preamble / 32) / 8;
+                                auto world_mesh_headers = read_vector<WorldMeshHeader>(file, number_of_headers);
+
+                                auto start = file.tellg();
+                                for (auto i = 0; i < number_of_headers; ++i)
+                                {
+                                    file.seekg(start + static_cast<std::streamoff>(world_mesh_headers[i].start - 40), std::ios::beg);
+
+                                    uint32_t number_of_indices = read<uint32_t>(file);
+                                    uint32_t separator = read<uint32_t>(file);
+                                    if (separator != 0xffffffff)
+                                    {
+                                        continue;
+                                    }
+
+                                    skip(file, 32);
+                                    uint32_t end_of_mesh = read<uint32_t>(file);
+                                    auto indices = read_vector<uint16_t>(file, number_of_indices);
+                                    for (auto j = 0u; j < indices.size(); j += 3)
+                                    {
+                                        drm->world_triangles.push_back({ indices[j], indices[j + 1u], indices[j + 2u] });
+                                    }
+                                }
+
+                                file.seekg(start + static_cast<std::streamoff>(header.length), std::ios::beg);
+                            }
+                            else
+                            {
+                                file.seekg(-4, std::ios::cur);
+                                skip(file, header.length);
+                            }
                         }
                         ++zero_sections;
                         break;
                     }
+                    case SectionType::Texture:
+                    {
+                        Texture texture;
+                        texture.id = header.id;
+                        skip(file, 4); // Skip PCD9
+
+                        auto format = read_vector<uint8_t>(file, 4);
+                        texture.format = std::string(format.begin(), format.end());
+
+                        uint32_t pixel_data_length = read<uint32_t>(file);
+                        skip(file, 4); // unknown uint32_t
+                        texture.width = read<uint16_t>(file);
+                        texture.height = read<uint16_t>(file);
+                        skip(file, 4); // unknown uint32_t
+                        texture.data = read_vector<uint8_t>(file, pixel_data_length);
+
+                        drm->textures[texture.id] = texture;
+                        break;
+                    }
                     default:
                     {
-                        skip(file, header.preamble / 32);
                         skip(file, header.length);
                         break;
                     }
                 }
+                ++section_index;
             }
 
             return drm;
--- trview.lau/drm.h
+++ trview.lau/drm.h
@@ -4,22 +4,62 @@
 #include <string>
 #include <cstdint>
 #include <vector>
+#include <unordered_map>
 
 namespace trview
 {
     namespace lau
     {
+        struct Vector3
+        {
+            float x;
+            float y;
+            float z;
+        };
+
         struct Vertex
         {
             int16_t x;
             int16_t y;
             int16_t z;
+            uint16_t a;
+            uint16_t b;
+            uint16_t c;
+            uint16_t d;
+            uint16_t e;
+            uint16_t f;
+            uint16_t g;
+        };
+
+        struct Triangle
+        {
+            uint16_t v0, v1, v2;
+        };
+
+        struct Texture
+        {
+            uint16_t id{ 0u };
+            std::string format;
+            uint16_t width{ 0u };
+            uint16_t height{ 0u };
+            std::vector<uint8_t> data;
+        };
+
+        struct FileHeader
+        {
+            uint16_t flags[4];
+            uint32_t id;
+            uint16_t id_of_next_section;
         };
 
         struct Drm
         {
             uint32_t version;
+            FileHeader file_header;
             std::vector<Vertex> world_mesh;
+            std::vector<Triangle> world_triangles;
+            std::unordered_map<uint16_t, Texture> textures;
+            Vector3 world_offset;
         };
 
         std::unique_ptr<Drm> load_drm(const std::wstring& filename);
