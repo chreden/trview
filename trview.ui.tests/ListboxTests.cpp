0e42409d0d4772b5a7d91a595461abeeaaed4b13
chreden <4263940+chreden@users.noreply.github.com>
2020-12-17 21:47:59 +0000

    Add some failing tests for the listbox
    Not yet implemented

--- trview.ui.tests/ListboxTests.cpp
+++ trview.ui.tests/ListboxTests.cpp
@@ -4,3 +4,50 @@
 using namespace trview;
 using namespace trview::ui;
 
+/// Tests that clicking on an item raises the item selected event.
+TEST(Listbox, ClickItemRaisesEvent)
+{
+    FAIL();
+}
+
+/// Tests that clicking on a checkbox raises the item state changed event.
+TEST(Listbox, ClickCheckboxRaisesEvent)
+{
+    FAIL();
+}
+
+/// Test that changing the selected item via keyboard input raises an event.
+TEST(Listbox, KeyboardNavigationRaisesSelectedItemEvent)
+{
+    FAIL();
+}
+
+/// Tests that deleting an item raises an event.
+TEST(Listbox, DeletingItemRaisesEvent)
+{
+    FAIL();
+}
+
+/// Tests that the correct columns are created based on constructor input.
+TEST(Listbox, ColumnsCreated)
+{
+    FAIL();
+}
+
+/// Tests that only key identity columns are used for selection comparison.
+TEST(Listbox, KeyIdentityUsedForSelection)
+{
+    FAIL();
+}
+
+/// Tests that ordering the listbox by a column functions.
+TEST(Listbox, OrderingByColumn)
+{
+    FAIL();
+}
+
+/// Checks that a checkbox is created when the column type is boolean.
+TEST(Listbox, CheckboxCreatedForBoolean)
+{
+    FAIL();
+}
