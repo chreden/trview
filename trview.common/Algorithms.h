3611ed94c90ad3205394e13b6c0502c42b9c3fd9
chreden <4263940+chreden@users.noreply.github.com>
2021-02-06 22:31:55 +0000

    Spacing

--- trview.common/Algorithms.h
+++ trview.common/Algorithms.h
@@ -2,10 +2,10 @@
 
 namespace trview
 {
-    template < typename T >
+    template <typename T>
     bool equals_any(T value, T other);
 
-    template < typename T, typename... Args >
+    template <typename T, typename... Args>
     bool equals_any(T value, T other, Args... set);
 }
 
