d84ef8a1446d7b3ed8643a2adb59cc39b45ae3f2
chreden <4263940+chreden@users.noreply.github.com>
2021-02-13 23:36:51 +0000

    Remove things not needed from stdafx.h
    #715

--- trview/stdafx.h
+++ trview/stdafx.h
@@ -5,28 +5,8 @@
 
 #pragma once
 
-#include <trview.app/Resources/targetver.h>
+#include <SDKDDKVer.h>
 
 #define NOMINMAX
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-
-// C RunTime Header Files
-#include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
-#include <tchar.h>
-
-#include <algorithm>
-#include <sstream>
-#include <vector>
-#include <cstdint>
-#include <string>
-#include <iomanip>
-#include <memory>
-
-#include <commdlg.h>
-#include <shellapi.h>
-#include <Shlwapi.h>
-
-#include <trview.common/Strings.h>
