296c16f5151c6cfc6f63f440697d4f67ace333ea
chreden <4263940+chreden@users.noreply.github.com>
2021-04-18 10:28:08 +0100

    Add a test for loading from entity or ai sources
    #724

--- trview.app.tests/Elements/LevelTests.cpp
+++ trview.app.tests/Elements/LevelTests.cpp
@@ -45,3 +45,41 @@ TEST(Level, LoadTypeNames)
         [](auto, auto) { return std::make_unique<MockMesh>(); }, [](auto&&, auto&&, auto&&, auto&&) { return std::make_shared<MockEntity>(); },
         [](auto&&, auto&&, auto&&, auto&) { return std::make_shared<MockEntity>(); });
 }
+
+TEST(Level, LoadFromEntitySources)
+{
+    tr2_entity entity;
+    entity.Room = 0;
+    entity.TypeID = 123;
+
+    tr4_ai_object ai_object{};
+
+    auto mock_level = std::make_unique<testing::NiceMock<MockLevel>>();
+    EXPECT_CALL(*mock_level, get_version).WillRepeatedly(Return(LevelVersion::Tomb4));
+    EXPECT_CALL(*mock_level, num_rooms()).WillRepeatedly(Return(1));
+    EXPECT_CALL(*mock_level, num_entities()).WillRepeatedly(Return(1));
+    EXPECT_CALL(*mock_level, get_entity(0)).WillRepeatedly(Return(entity));
+    EXPECT_CALL(*mock_level, num_ai_objects()).WillRepeatedly(Return(1));
+    EXPECT_CALL(*mock_level, get_ai_object(0)).WillRepeatedly(Return(ai_object));
+
+    uint32_t entity_source_called = 0;
+    uint32_t ai_source_called = 0;
+
+    Level level(std::make_shared<MockDevice>(), std::make_shared<MockShaderStorage>(), std::move(mock_level),
+        std::make_unique<MockLevelTextureStorage>(), std::make_unique<MockMeshStorage>(), std::make_unique<MockTransparencyBuffer>(),
+        std::make_unique<MockSelectionRenderer>(), std::make_shared<MockTypeNameLookup>(), [](auto, auto, auto, auto, auto) {return std::make_unique<MockMesh>(); },
+        [](auto, auto) { return std::make_unique<MockMesh>(); }, 
+        [&](auto&&, auto&&, auto&&, auto&&) 
+        { 
+            ++entity_source_called;
+            return std::make_shared<MockEntity>(); 
+        },
+        [&](auto&&, auto&&, auto&&, auto&) 
+        { 
+            ++ai_source_called;
+            return std::make_shared<MockEntity>(); 
+        });
+
+    ASSERT_EQ(entity_source_called, 1);
+    ASSERT_EQ(ai_source_called, 1);
+}
