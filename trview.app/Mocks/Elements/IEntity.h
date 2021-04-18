43715cf9554e03e2ee2dd0f5495fc560883e1884
chreden <4263940+chreden@users.noreply.github.com>
2021-04-18 10:32:01 +0100

    Add final
    #724

--- trview.app/Mocks/Elements/IEntity.h
+++ trview.app/Mocks/Elements/IEntity.h
@@ -6,7 +6,7 @@ namespace trview
 {
     namespace mocks
     {
-        struct MockEntity : public IEntity
+        struct MockEntity final : public IEntity
         {
             virtual ~MockEntity() = default;
             MOCK_METHOD(void, render, (const ICamera&, const ILevelTextureStorage&, const DirectX::SimpleMath::Color&));
