2d2f6ebe0ec24224607bfa24fcb2b025d93b1c14
chreden <4263940+chreden@users.noreply.github.com>
2021-04-29 22:32:31 +0100

    Add override to IRoom, remove comments
    Comments are in the interface
    #758

--- trview.app/Elements/Room.h
+++ trview.app/Elements/Room.h
@@ -5,33 +5,22 @@
 #include <set>
 #include <memory>
 #include <optional>
-
 #include <DirectXCollision.h>
 #include <SimpleMath.h>
 
-#include <trview.graphics/Texture.h>
-
 #include <trlevel/trtypes.h>
 #include <trlevel/ILevel.h>
-
-#include "StaticMesh.h"
 #include <trview.app/Geometry/TransparencyBuffer.h>
 #include <trview.app/Geometry/IMesh.h>
 #include <trview.app/Elements/Sector.h>
 #include <trview.app/Geometry/PickResult.h>
+#include <trview.graphics/Texture.h>
+#include "StaticMesh.h"
 #include "IRoom.h"
 
 namespace trview
 {
-    struct ILevelTextureStorage;
-    struct IMeshStorage;
-    struct IEntity;
-    struct ICamera;
-    class Mesh;
-    class TransparencyBuffer;
-    struct ILevel;
-
-    class Room : public IRoom
+    class Room final : public IRoom
     {
     public:
         explicit Room(const IMesh::Source& mesh_source,
@@ -44,60 +33,30 @@ namespace trview
 
         Room(const Room&) = delete;
         Room& operator=(const Room&) = delete;
-
         virtual RoomInfo info() const override;
         virtual std::set<uint16_t> neighbours() const override;
-
         virtual PickResult pick(const DirectX::SimpleMath::Vector3& position, const DirectX::SimpleMath::Vector3& direction, bool include_entities, bool include_triggers, bool include_hidden_geometry = false, bool include_room_geometry = true) const override;
-
-        // Render the level geometry and the objects contained in this room.
-        // camera: The camera to use to render.
-        // texture_storage: The textures for the level.
-        // selected: The selection mode to use to highlight geometry and objects.
         virtual void render(const ICamera& camera, const ILevelTextureStorage& texture_storage, SelectionMode selected, bool show_hidden_geometry, bool show_water) override;
-
         virtual void render_contained(const ICamera& camera, const ILevelTextureStorage& texture_storage, SelectionMode selected, bool show_water, bool force_water = false) override;
-
-        // Add the specified entity to the room.
-        // Entity: The entity to add.
         virtual void add_entity(IEntity* entity) override;
-
-        /// Add the specified trigger to the room.
-        /// @paramt trigger The trigger to add.
         virtual void add_trigger(Trigger* trigger) override;
-
         virtual const std::vector<std::shared_ptr<Sector>> sectors() const override;
         virtual void get_transparent_triangles(ITransparencyBuffer& transparency, const ICamera& camera, SelectionMode selected, bool include_triggers, bool show_water) override;
-
         virtual void get_contained_transparent_triangles(ITransparencyBuffer& transparency, const ICamera& camera, SelectionMode selected, bool show_water, bool force_water = false) override;
-
         virtual AlternateMode alternate_mode() const override;
         virtual int16_t alternate_room() const override;
         virtual int16_t alternate_group() const override;
-
-        // Set this room to be the alternate room of the room specified.
-        // This will change the alternate_mode of this room to IsAlternate.
-        // number: The room number.
         virtual void set_is_alternate(int16_t number) override;
-
         virtual uint16_t num_x_sectors() const override;
         virtual uint16_t num_z_sectors() const override;
-
         virtual DirectX::SimpleMath::Vector3 centre() const override;
-
         virtual DirectX::BoundingBox bounding_box() const override;
-
         virtual void generate_trigger_geometry() override;
-
         virtual uint32_t number() const override;
         virtual void update_bounding_box() override;
-
         virtual bool outside() const override;
-
         virtual bool water() const override;
-
         virtual bool quicksand() const override;
-
         virtual Trigger* trigger_at(int32_t x, int32_t z) const override;
     private:
         void generate_geometry(trlevel::LevelVersion level_version, const IMesh::Source& mesh_source, const trlevel::tr3_room& room, const ILevelTextureStorage& texture_storage);
--- trview.app/Mocks/Elements/IRoom.h
+++ trview.app/Mocks/Elements/IRoom.h
@@ -9,31 +9,31 @@ namespace trview
         struct MockRoom final : public IRoom
         {
             virtual ~MockRoom() = default;
-            MOCK_METHOD(void, add_entity, (IEntity*));
-            MOCK_METHOD(void, add_trigger, (Trigger*));
-            MOCK_METHOD(int16_t, alternate_group, (), (const));
-            MOCK_METHOD(AlternateMode, alternate_mode, (), (const));
-            MOCK_METHOD(int16_t, alternate_room, (), (const));
-            MOCK_METHOD(DirectX::BoundingBox, bounding_box, (), (const));
-            MOCK_METHOD(DirectX::SimpleMath::Vector3, centre, (), (const));
-            MOCK_METHOD(void, generate_trigger_geometry, ());
-            MOCK_METHOD(void, get_contained_transparent_triangles, (ITransparencyBuffer&, const ICamera&, SelectionMode, bool, bool));
-            MOCK_METHOD(void, get_transparent_triangles, (ITransparencyBuffer&, const ICamera&, SelectionMode, bool, bool));
-            MOCK_METHOD(RoomInfo, info, (), (const));
-            MOCK_METHOD(std::set<uint16_t>, neighbours, (), (const));
-            MOCK_METHOD(uint16_t, num_x_sectors, (), (const));
-            MOCK_METHOD(uint16_t, num_z_sectors, (), (const));
-            MOCK_METHOD(uint32_t, number, (), (const));
-            MOCK_METHOD(bool, outside, (), (const));
-            MOCK_METHOD(PickResult, pick, (const DirectX::SimpleMath::Vector3&, const DirectX::SimpleMath::Vector3&, bool, bool, bool, bool), (const));
-            MOCK_METHOD(bool, quicksand, (), (const));
-            MOCK_METHOD(void, render, (const ICamera&, const ILevelTextureStorage&, SelectionMode, bool, bool));
-            MOCK_METHOD(void, render_contained, (const ICamera&, const ILevelTextureStorage&, SelectionMode, bool, bool));;
-            MOCK_METHOD(const std::vector<std::shared_ptr<Sector>>, sectors, (), (const));
-            MOCK_METHOD(void, set_is_alternate, (int16_t));
-            MOCK_METHOD(Trigger*, trigger_at, (int32_t, int32_t), (const));
-            MOCK_METHOD(void, update_bounding_box, ());
-            MOCK_METHOD(bool, water, (), (const));
+            MOCK_METHOD(void, add_entity, (IEntity*), (override));
+            MOCK_METHOD(void, add_trigger, (Trigger*), (override));
+            MOCK_METHOD(int16_t, alternate_group, (), (const, override));
+            MOCK_METHOD(AlternateMode, alternate_mode, (), (const, override));
+            MOCK_METHOD(int16_t, alternate_room, (), (const, override));
+            MOCK_METHOD(DirectX::BoundingBox, bounding_box, (), (const, override));
+            MOCK_METHOD(DirectX::SimpleMath::Vector3, centre, (), (const, override));
+            MOCK_METHOD(void, generate_trigger_geometry, (), (override));
+            MOCK_METHOD(void, get_contained_transparent_triangles, (ITransparencyBuffer&, const ICamera&, SelectionMode, bool, bool), (override));
+            MOCK_METHOD(void, get_transparent_triangles, (ITransparencyBuffer&, const ICamera&, SelectionMode, bool, bool), (override));
+            MOCK_METHOD(RoomInfo, info, (), (const, override));
+            MOCK_METHOD(std::set<uint16_t>, neighbours, (), (const, override));
+            MOCK_METHOD(uint16_t, num_x_sectors, (), (const, override));
+            MOCK_METHOD(uint16_t, num_z_sectors, (), (const, override));
+            MOCK_METHOD(uint32_t, number, (), (const, override));
+            MOCK_METHOD(bool, outside, (), (const, override));
+            MOCK_METHOD(PickResult, pick, (const DirectX::SimpleMath::Vector3&, const DirectX::SimpleMath::Vector3&, bool, bool, bool, bool), (const, override));
+            MOCK_METHOD(bool, quicksand, (), (const, override));
+            MOCK_METHOD(void, render, (const ICamera&, const ILevelTextureStorage&, SelectionMode, bool, bool), (override));
+            MOCK_METHOD(void, render_contained, (const ICamera&, const ILevelTextureStorage&, SelectionMode, bool, bool), (override));;
+            MOCK_METHOD(const std::vector<std::shared_ptr<Sector>>, sectors, (), (const, override));
+            MOCK_METHOD(void, set_is_alternate, (int16_t), (override));
+            MOCK_METHOD(Trigger*, trigger_at, (int32_t, int32_t), (const, override));
+            MOCK_METHOD(void, update_bounding_box, (), (override));
+            MOCK_METHOD(bool, water, (), (const, override));
         };
     }
 }
