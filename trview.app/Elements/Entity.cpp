20a947ea368abad9dad5fc6eb151493328985f97
chreden <4263940+chreden@users.noreply.github.com>
2021-05-26 21:38:40 +0100

    First attempt at fixing OCB adjustment
    Seems to work in TR4 but TR5 is still a little bit of a disaster.

--- trview.app/Elements/Entity.cpp
+++ trview.app/Elements/Entity.cpp
@@ -300,22 +300,27 @@ namespace trview
 
     void Entity::apply_ocb_adjustment(uint32_t ocb)
     {
-        using namespace DirectX::SimpleMath;
-        const int flags = ocb & 0x3F;
-        // This assumes that pickups only have one mesh, as a general rule (it seems to work most of the time).
-        // Applying OCB adjustment without this test when OCB is 0 makes Lara and other entities move up, which
-        // isn't right.
-        if (_meshes.size() == 1 && equals_any(flags, 0, 3, 7, 11))
+        // If this isn't a pickup, don't adjust the position.
+        if (!has_any(ocb, 64)) 
         {
-            Matrix offset = Matrix::CreateTranslation(0, -_bounding_box.Extents.y, 0);
-            _world *= offset;
-
-            for (auto& obb : _oriented_boxes)
-            {
-                obb.Transform(obb, offset);
-            }
-            _bounding_box.Transform(_bounding_box, offset);
+            return;
         }
+
+        const int flags = ocb & 0x3F;
+        if (!equals_any(flags, 0, 3, 7, 11))
+        {
+            return;
+        }
+
+        using namespace DirectX::SimpleMath;
+        Matrix offset = Matrix::CreateTranslation(0, -_bounding_box.Extents.y, 0);
+        _world *= offset;
+
+        for (auto& obb : _oriented_boxes)
+        {
+            obb.Transform(obb, offset);
+        }
+        _bounding_box.Transform(_bounding_box, offset);
     }
 
     DirectX::BoundingBox Entity::bounding_box() const
--- trview.common/Algorithms.h
+++ trview.common/Algorithms.h
@@ -7,6 +7,12 @@ namespace trview
 
     template <typename T1, typename T2, typename... Args>
     bool equals_any(T1&& value, T2&& other, Args&&... set);
+
+    template <typename T1, typename T2>
+    bool has_any(T1&& value, T2&& other);
+
+    template <typename T1, typename T2, typename... Args>
+    bool has_any(T1&& value, T2&& other, Args&&... set);
 }
 
 #include "Algorithms.hpp"
