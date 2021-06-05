daf09fe8ce96955f182154905af78c9deae2132f
chreden <4263940+chreden@users.noreply.github.com>
2021-06-05 22:49:49 +0100

    Create empty action enumeration
    #776

---
+++ trview.app/Routing/Action.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace trview
+{
+    /// <summary>
+    /// Actions that can be performed at a waypoint node in a route.
+    /// </summary>
+    enum class Action
+    {
+
+    };
+}
