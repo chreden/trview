736494dc6e417e9d82580f2de9b2743903569e6a
chreden <4263940+chreden@users.noreply.github.com>
2021-06-08 22:15:22 +0100

    Add actions to di
    #776

--- trview.app/Routing/di.hpp
+++ trview.app/Routing/di.hpp
@@ -2,6 +2,7 @@
 
 #include <external/boost/di.hpp>
 #include "Route.h"
+#include "Actions.h"
 
 namespace trview
 {
@@ -9,6 +10,12 @@ namespace trview
     {
         using namespace boost;
         return di::make_injector(
+            di::bind<IActions>.to(
+                []()
+                {
+                    Resource type_list = get_resource_memory(IDR_ACTIONS, L"TEXT");
+                    return std::make_shared<Actions>(std::string(type_list.data, type_list.data + type_list.size));
+                }),
             di::bind<IRoute>.to<Route>(),
             di::bind<IRoute::Source>.to(
                 [](const auto& injector) -> IRoute::Source
