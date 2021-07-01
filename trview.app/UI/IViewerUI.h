7b83f3f15ac3f47195aac6bf121db5524be5bb6d
chreden <4263940+chreden@users.noreply.github.com>
2021-07-01 10:29:28 +0100

    Update mock, add a comment
    #633

--- trview.app/UI/IViewerUI.h
+++ trview.app/UI/IViewerUI.h
@@ -104,6 +104,9 @@ namespace trview
         /// Event raised when user edits camera position.
         Event<DirectX::SimpleMath::Vector3> on_camera_position;
 
+        /// <summary>
+        /// Event raised when the camera rotation is changed.
+        /// </summary>
         Event<float, float> on_camera_rotation;
 
         /// Event raised when user enters a command.
