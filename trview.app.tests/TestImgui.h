2dd1ca44a3a3aad0d7462ee261c852e68f685d77
chreden <4263940+chreden@users.noreply.github.com>
2022-03-12 19:48:17 +0000

    Restore Settings Window slider tests
    Restore the slider tests for the settings window - they were commented out as I couldn't figure out how to do it.
    Add text logging from sliders and add a mouse down instead of a mouse click in the imgui test class.
    Closes #895

--- trview.app.tests/TestImgui.h
+++ trview.app.tests/TestImgui.h
@@ -22,6 +22,7 @@ namespace trview
             explicit TestImgui(const RenderCallback& callback);
             ~TestImgui();
 
+            void mouse_down_element(TestImGuiId id, TestImGuiId active_override = TestImGuiId());
             void click_element(TestImGuiId id, bool show_context_menu = false, TestImGuiId active_override = TestImGuiId());
             void click_element_with_hover(TestImGuiId id, bool show_context_menu = false, TestImGuiId active_override = TestImGuiId());
             void enter_text(const std::string& text);
