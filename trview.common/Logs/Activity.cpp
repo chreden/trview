6c71dc76525812b2520f4fa1bbec9f704fb06246
chreden <4263940+chreden@users.noreply.github.com>
2022-05-10 10:40:26 +0100

    Add activity start on child activity
    #240

--- trview.common/Logs/Activity.cpp
+++ trview.common/Logs/Activity.cpp
@@ -6,13 +6,14 @@ namespace trview
     Activity::Activity(const std::shared_ptr<ILog>& log, const std::string& topic, const std::string& name)
         : _log(log), _topic(topic), _names({ name })
     {
-        _log->log(topic, name, "Activity Started");
+        _log->log(_topic, _names, "Activity Started");
     }
 
     Activity::Activity(const Activity& parent, const std::string& name)
         : _log(parent._log), _topic(parent._topic), _names(parent._names)
     {
         _names.push_back(name);
+        _log->log(_topic, _names, "Activity Started");
     }
 
     Activity::~Activity()
