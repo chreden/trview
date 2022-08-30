2cb30ad70d90e7d68ba390ae63083e0941b903d7
chreden <4263940+chreden@users.noreply.github.com>
2022-08-30 22:45:39 +0100

    Add tests for decrypter
    #1000

--- trlevel.tests/DecrypterTests.cpp
+++ trlevel.tests/DecrypterTests.cpp
@@ -1,6 +1,25 @@
 #include <trlevel/Decrypter.h>
+#include <trview.common/Resources.h>
+#include "resource.h"
+
+using namespace trlevel;
+
+namespace
+{
+    std::vector<uint8_t> get_resource(int id)
+    {
+        auto resource = trview::get_resource_memory(id, L"FILE");
+        return std::vector<uint8_t>(resource.data, resource.data + resource.size);
+    }
+}
 
 TEST(Decrypter, DecryptsFile)
 {
-    FAIL();
+    auto encrypted = get_resource(IDR_ENCRYPTED_LAKE);
+    auto expected = get_resource(IDR_ORIGINAL_LAKE);
+
+    Decrypter decrypter;
+    decrypter.decrypt(encrypted);
+
+    ASSERT_EQ(encrypted, expected);
 }
