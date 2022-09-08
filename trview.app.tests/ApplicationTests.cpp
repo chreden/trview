6934a3353046097eeb6451710f7fa1667a64819a
chreden <4263940+chreden@users.noreply.github.com>
2022-09-08 09:51:48 +0100

    Add test for route clearing
    #1004

--- trview.app.tests/ApplicationTests.cpp
+++ trview.app.tests/ApplicationTests.cpp
@@ -760,6 +760,8 @@ TEST(Application, RecentRouteLoaded)
     auto route = mock_shared<MockRoute>();
     auto [viewer_ptr, viewer] = create_mock<MockViewer>();
     EXPECT_CALL(viewer, set_route(std::shared_ptr<IRoute>(route))).Times(1);
+    UserSettings called_settings{};
+    EXPECT_CALL(viewer, set_settings).Times(AtLeast(1)).WillRepeatedly(SaveArg<0>(&called_settings));
     auto [level_ptr, level] = create_mock<trview::mocks::MockLevel>();
     ON_CALL(level, filename).WillByDefault(Return("test.tr2"));
 
@@ -773,4 +775,31 @@ TEST(Application, RecentRouteLoaded)
     
     application->open("test.tr2", ILevel::OpenMode::Full);
 
+    ASSERT_EQ(called_settings.recent_routes["test.tr2"].route_path, "test.tvr");
+}
+
+TEST(Application, RecentRouteNotLoaded)
+{
+    UserSettings settings;
+    settings.recent_routes["test.tr2"] = { "test.tvr", false };
+    auto [settings_loader_ptr, settings_loader] = create_mock<MockSettingsLoader>();
+    ON_CALL(settings_loader, load_user_settings).WillByDefault(Return(settings));
+    auto dialogs = mock_shared<MockDialogs>();
+    EXPECT_CALL(*dialogs, message_box(std::wstring(L"Reopen last used route for this level?"), std::wstring(L"Reopen route"), IDialogs::Buttons::Yes_No)).Times(1).WillOnce(Return(false));
+    auto [viewer_ptr, viewer] = create_mock<MockViewer>();
+    UserSettings called_settings{};
+    EXPECT_CALL(viewer, set_settings).Times(AtLeast(1)).WillRepeatedly(SaveArg<0>(&called_settings));
+    auto [level_ptr, level] = create_mock<trview::mocks::MockLevel>();
+    ON_CALL(level, filename).WillByDefault(Return("test.tr2"));
+
+    auto application = register_test_module()
+        .with_settings_loader(std::move(settings_loader_ptr))
+        .with_dialogs(dialogs)
+        .with_viewer(std::move(viewer_ptr))
+        .with_level_source([&](auto&&...) { return std::move(level_ptr); })
+        .build();
+
+    application->open("test.tr2", ILevel::OpenMode::Full);
+
+    ASSERT_TRUE(called_settings.recent_routes.empty());
 }
