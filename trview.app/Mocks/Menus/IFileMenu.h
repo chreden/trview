8fd830edf07b235d9eb3821a1f0eb4eaa9c78f48
chreden <4263940+chreden@users.noreply.github.com>
2023-07-09 23:59:50 +0100

    Update IFileMenu.h

--- trview.app/Mocks/Menus/IFileMenu.h
+++ trview.app/Mocks/Menus/IFileMenu.h
@@ -13,6 +13,7 @@ namespace trview
             MOCK_METHOD(std::vector<std::string>, local_levels, (), (const, override));
             MOCK_METHOD(void, open_file, (const std::string&), (override));
             MOCK_METHOD(void, set_recent_files, (const std::list<std::string>&), (override));
+            MOCK_METHOD(void, switch_to, (const std::string&), (override));
         };
     }
 }
