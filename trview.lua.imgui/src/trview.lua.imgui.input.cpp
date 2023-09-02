d2f570d1c4f76260646747540a85ef8d7659c318
chreden <4263940+chreden@users.noreply.github.com>
2023-09-02 14:25:50 +0100

    Add InputFloat
    #1129

--- trview.lua.imgui/src/trview.lua.imgui.input.cpp
+++ trview.lua.imgui/src/trview.lua.imgui.input.cpp
@@ -8,6 +8,16 @@ namespace trview
     {
         namespace
         {
+            int input_float(lua_State* L)
+            {
+                const auto label = get_string(L, 1, "label");
+                float out = 0.0f;
+                bool result = ImGui::InputFloat(label.c_str(), &out);
+                lua_pushboolean(L, result);
+                lua_pushnumber(L, out);
+                return 2;
+            }
+
             int input_int(lua_State* L)
             {
                 const auto label = get_string(L, 1, "label");
@@ -32,6 +42,7 @@ namespace trview
         void register_input(lua_State* L)
         {
             constexpr luaL_Reg funcs[] = {
+                { "InputFloat", input_float },
                 { "InputInt", input_int },
                 { "InputText", input_text },
                 { nullptr, nullptr }
