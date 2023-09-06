176ece6a0b227b1ec45e7e5698af0cbb8ba3e6f6
chreden <4263940+chreden@users.noreply.github.com>
2023-09-06 10:17:41 +0100

    Input stubs

--- trview.lua.imgui/src/trview.lua.imgui.input.cpp
+++ trview.lua.imgui/src/trview.lua.imgui.input.cpp
@@ -8,6 +8,11 @@ namespace trview
     {
         namespace
         {
+            int input_double(lua_State* L)
+            {
+                return 0;
+            }
+
             int input_float(lua_State* L)
             {
                 const auto label = get_string(L, 1, "label");
@@ -18,6 +23,21 @@ namespace trview
                 return 2;
             }
 
+            int input_float2(lua_State* L)
+            {
+                return 0;
+            }
+
+            int input_float3(lua_State* L)
+            {
+                return 0;
+            }
+
+            int input_float4(lua_State* L)
+            {
+                return 0;
+            }
+
             int input_int(lua_State* L)
             {
                 const auto label = get_string(L, 1, "label");
@@ -26,6 +46,31 @@ namespace trview
                 lua_pushboolean(L, result);
                 lua_pushinteger(L, out);
                 return 2;
+            }
+
+            int input_int2(lua_State* L)
+            {
+                return 0;
+            }
+
+            int input_int3(lua_State* L)
+            {
+                return 0;
+            }
+
+            int input_int4(lua_State* L)
+            {
+                return 0;
+            }
+
+            int input_scalar(lua_State* L)
+            {
+                return 0;
+            }
+
+            int input_scalarN(lua_State* L)
+            {
+                return 0;
             }
 
             int input_text(lua_State* L)
@@ -42,8 +87,17 @@ namespace trview
         void register_input(lua_State* L)
         {
             constexpr luaL_Reg funcs[] = {
+                { "InputDouble", input_double },
                 { "InputFloat", input_float },
+                { "InputFloat2", input_float2 },
+                { "InputFloat3", input_float3 },
+                { "InputFloat4", input_float4 },
                 { "InputInt", input_int },
+                { "InputInt2", input_int2 },
+                { "InputInt3", input_int3 },
+                { "InputInt4", input_int4 },
+                { "InputScalar", input_scalar },
+                { "InputScalarN", input_scalarN },
                 { "InputText", input_text },
                 { nullptr, nullptr }
             };
