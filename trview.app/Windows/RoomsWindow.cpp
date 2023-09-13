a4f8a0b0c3e957f54f8eec385e9b9fd6e0a41f62
chreden <4263940+chreden@users.noreply.github.com>
2023-09-13 02:01:50 +0100

    Add statics count to room list

--- trview.app/Windows/RoomsWindow.cpp
+++ trview.app/Windows/RoomsWindow.cpp
@@ -294,11 +294,12 @@ namespace trview
                 set_sync_room(sync_room);
             }
 
-            if (ImGui::BeginTable(Names::rooms_list.c_str(), 4, ImGuiTableFlags_Sortable | ImGuiTableFlags_ScrollY | ImGuiTableFlags_SizingFixedSame, ImVec2(-1, -1)))
+            if (ImGui::BeginTable(Names::rooms_list.c_str(), 5, ImGuiTableFlags_Sortable | ImGuiTableFlags_ScrollY | ImGuiTableFlags_SizingFixedFit, ImVec2(-1, -1)))
             {
                 ImGui::TableSetupColumn("#");
                 ImGui::TableSetupColumn("Items");
                 ImGui::TableSetupColumn("Triggers");
+                ImGui::TableSetupColumn("Statics");
                 ImGui::TableSetupColumn("Hide");
                 ImGui::TableSetupScrollFreeze(1, 1);
                 ImGui::TableHeadersRow();
@@ -320,11 +321,14 @@ namespace trview
                     return room.triggers().size();
                 };
 
+                auto static_mesh_count = [](const IRoom& room) { return room.static_meshes().size(); };
+
                 imgui_sort_weak(_all_rooms,
                     {
                         [](auto&& l, auto&& r) { return l.number() < r.number(); },
                         [&](auto&& l, auto&& r) { return item_count(l) < item_count(r); },
                         [&](auto&& l, auto&& r) { return trigger_count(l) < trigger_count(r); },
+                        [&](auto&& l, auto&& r) { return static_mesh_count(l) < static_mesh_count(r); },
                         [&](auto&& l, auto&& r) { return l.visible() < r.visible(); }
                     }, _force_sort);
 
@@ -364,6 +368,8 @@ namespace trview
                     ImGui::Text(std::to_string(item_count(*room_ptr)).c_str());
                     ImGui::TableNextColumn();
                     ImGui::Text(std::to_string(trigger_count(*room_ptr)).c_str());
+                    ImGui::TableNextColumn();
+                    ImGui::Text(std::to_string(static_mesh_count(*room_ptr)).c_str());
                     ImGui::TableNextColumn();
                     bool hidden = !room_ptr->visible();
                     ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, 0));
@@ -521,7 +527,7 @@ namespace trview
                             _in_floordata_mode = false;
                         }
 
-                        if (ImGui::BeginTabItem("Statics"))
+                        if (ImGui::BeginTabItem("Statics##Statics Tab"))
                         {
                             render_statics_tab();
                             ImGui::EndTabItem();
