529d711134b940f504898593ef0425b3ffb12207
chreden <4263940+chreden@users.noreply.github.com>
2024-03-12 11:59:25 +0000

    Revert changes to app.tests main

--- trview.app.tests/Main.cpp
+++ trview.app.tests/Main.cpp
@@ -1,6 +1,4 @@
 #include "gtest/gtest.h"
-// #include <imgui_te_engine.h>
-// #include <imgui_app.h>
 
 void ImGuiTrviewTestEngineHook_ItemText(ImGuiContext* ctx, ImGuiID id, const char* buf)
 {
@@ -10,76 +8,8 @@ void ImGuiTrviewTestEngineHook_RenderedText(ImGuiContext* ctx, ImGuiID id, const
 {
 }
 
-
 int wmain(int argc, wchar_t** argv)
 {
-    /*
-    ImGuiApp* app = ImGuiApp_ImplDefault_Create();
-
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-
-    app->DpiAware = false;
-    app->SrgbFramebuffer = false;
-    app->ClearColor = ImVec4(0.120f, 0.120f, 0.120f, 1.000f);
-    app->InitCreateWindow(app, "Dear ImGui: Minimal App With Test Engine", ImVec2(1600, 1000));
-    app->InitBackends(app);
-
-    ImGuiTestEngine* engine = ImGuiTestEngine_CreateContext();
-    ImGuiTestEngineIO& test_io = ImGuiTestEngine_GetIO(engine);
-    test_io.ConfigVerboseLevel = ImGuiTestVerboseLevel_Info;
-    test_io.ConfigVerboseLevelOnError = ImGuiTestVerboseLevel_Debug;
-    test_io.ConfigRunSpeed = ImGuiTestRunSpeed_Cinematic; // Default to slowest mode in this demo
-    test_io.ScreenCaptureFunc = ImGuiApp_ScreenCaptureFunc;
-    test_io.ScreenCaptureUserData = (void*)app;
-    test_io.ConfigSavedSettings = false;
-
-    ImGuiTestEngine_Start(engine, ImGui::GetCurrentContext());
-    ImGuiTestEngine_InstallDefaultCrashHandler();
-
-    // Main loop
-    bool aborted = false;
-    while (!aborted)
-    {
-        if (!aborted && !app->NewFrame(app))
-            aborted = true;
-        if (app->Quit)
-            aborted = true;
-
-        if (aborted && ImGuiTestEngine_TryAbortEngine(engine))
-            break;
-
-        ImGui::NewFrame();
-
-        // Show windows
-        ImGui::ShowDemoWindow();
-        // ImGuiTestEngine_ShowTestEngineWindows(engine, NULL);
-
-        // Render and swap
-        app->Vsync = test_io.IsRequestingMaxAppSpeed ? false : true;
-        ImGui::Render();
-        app->Render(app);
-
-        // Post-swap handler is REQUIRED in order to support screen capture
-        ImGuiTestEngine_PostSwap(engine);
-    }
-
-    // Shutdown
-    ImGuiTestEngine_Stop(engine);
-    app->ShutdownBackends(app);
-    app->ShutdownCloseWindow(app);
-    ImGui::DestroyContext();
-
-    // IMPORTANT: we need to destroy the Dear ImGui context BEFORE the test engine context, so .ini data may be saved.
-    ImGuiTestEngine_DestroyContext(engine);
-
-    app->Destroy(app);*/
-
     ::testing::InitGoogleTest(&argc, argv);
-    //testing::FLAGS_gtest_catch_exceptions
-    GTEST_FLAG_SET(catch_exceptions, false);
     return RUN_ALL_TESTS();
 }
