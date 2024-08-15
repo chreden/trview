42ffd54b00890454f59136cbdd908c13f8351746
chreden <4263940+chreden@users.noreply.github.com>
2024-08-15 02:11:56 +0100

    Load all default libs
    Load all default libs including os.
    Closes #1267

--- trview.app/Lua/Lua.cpp
+++ trview.app/Lua/Lua.cpp
@@ -57,10 +57,13 @@ namespace trview
           {LUA_LOADLIBNAME, luaopen_package},
           {LUA_COLIBNAME, luaopen_coroutine},
           {LUA_TABLIBNAME, luaopen_table},
+          {LUA_IOLIBNAME, luaopen_io},
+          {LUA_OSLIBNAME, luaopen_os},
           {LUA_STRLIBNAME, luaopen_string},
           {LUA_MATHLIBNAME, luaopen_math},
           {LUA_UTF8LIBNAME, luaopen_utf8},
-          {LUA_DBLIBNAME, luaopen_debug}
+          {LUA_DBLIBNAME, luaopen_debug},
+          {NULL, NULL}
         };
     }
 
