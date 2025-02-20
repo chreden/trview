56e767f4a71c1c7994ab4805ac05fd4ab351e506
chreden <4263940+chreden@users.noreply.github.com>
2025-02-20 12:16:31 +0000

    Update PluginsTests.cpp

--- trview.app.tests/Plugins/PluginsTests.cpp
+++ trview.app.tests/Plugins/PluginsTests.cpp
@@ -46,3 +46,18 @@ TEST(Plugins, Initialise)
     EXPECT_CALL(plugin, initialise(application.get())).Times(1);
     plugins.initialise(application.get());
 }
+
+TEST(Plugins, SettingsApplied)
+{
+    auto files = mock_shared<MockFiles>();
+    auto plugin = mock_shared<MockPlugin>();
+    EXPECT_CALL(*plugin, set_enabled(false)).Times(1);
+
+    auto source = [&](auto&&...) { return plugin; };
+    UserSettings settings{ .plugins = { { "plugindir", {.enabled = false } } } };
+    settings.plugin_directories.push_back("dir");
+    EXPECT_CALL(*files, get_directories("dir"))
+        .WillRepeatedly(testing::Return(std::vector<IFiles::Directory>{ { "plugindir", "plugindir_friendly" } }));
+
+    Plugins plugins(files, mock_shared<MockPlugin>(), source, settings);
+}
