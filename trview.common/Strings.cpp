f28a59f82a1e71b3f43bf9f7e89e8e8ad17e48d9
chreden <4263940+chreden@users.noreply.github.com>
2025-03-31 09:22:40 +0100

    Use std::filesystem

--- trview.common/Strings.cpp
+++ trview.common/Strings.cpp
@@ -1,6 +1,7 @@
 #include "Strings.h"
 #include <algorithm>
 #include <format>
+#include <filesystem>
 
 namespace trview
 {
@@ -45,19 +46,11 @@ namespace trview
 
     std::string filename_without_path(const std::string& filename)
     {
-        const auto last_fs = filename.find_last_of('/');
-        const auto last_bs = filename.find_last_of('\\');
-        const auto last_index = 
-            last_fs == filename.npos ?
-            last_bs : 
-            last_bs == filename.npos ?
-            last_fs : std::max(last_fs, last_bs);
-        return last_index == filename.npos ? filename : filename.substr(std::min(last_index + 1, filename.size()));
+        return std::filesystem::path(filename).filename().string();
     }
 
     std::string path_for_filename(const std::string& filename)
     {
-        auto last_index = std::min(filename.find_last_of('\\'), filename.find_last_of('/'));
-        return last_index == filename.npos ? std::string() : filename.substr(0, std::min(last_index, filename.size()));
+        return std::filesystem::path(filename).parent_path().string();
     }
 }
