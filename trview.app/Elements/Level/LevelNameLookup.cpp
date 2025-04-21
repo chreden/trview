b5650f8a32252c9e4e15a5b389f85a2655fbc247
chreden <4263940+chreden@users.noreply.github.com>
2025-04-21 23:37:56 +0100

    Level names for 1-3

--- trview.app/Elements/Level/LevelNameLookup.cpp
+++ trview.app/Elements/Level/LevelNameLookup.cpp
@@ -2,9 +2,101 @@
 #include "../ILevel.h"
 
 #include <filesystem>
+#include <spanstream>
+#include <ranges>
+
+#include <trlevel/Level_common.h>
 
 namespace trview
 {
+    namespace
+    {
+        std::optional<std::vector<uint8_t>> load_strings_data(const std::shared_ptr<IFiles>& files, const std::filesystem::path& level_path)
+        {
+            if (const auto strings_data = files->load_file(level_path.parent_path() /= "../TEXT/EN/STRINGS.TXT"))
+            {
+                return strings_data;
+            }
+
+            if (const auto expansion_strings_data = files->load_file(level_path.parent_path() /= "../../TEXT/EN/STRINGS.TXT"))
+            {
+                return expansion_strings_data;
+            }
+
+            return std::nullopt;
+        }
+
+        std::optional<std::string> read_tombpc_1_3(const std::shared_ptr<IFiles>& files, const std::string& level_filename)
+        {
+            const std::filesystem::path level_path{ level_filename };
+            if (const auto tombpc_data = files->load_file(level_path.parent_path() /= "TOMBPC.DAT"))
+            {
+                try
+                {
+                    std::basic_ispanstream<uint8_t> file{ { *tombpc_data } };
+                    file.exceptions(std::ios::failbit | std::ios::badbit | std::ios::eofbit);
+                    file.seekg(326);
+
+                    using namespace trlevel;
+
+                    uint16_t num_levels = read<uint16_t>(file);
+                    uint16_t num_chapters = read<uint16_t>(file);
+                    uint16_t num_titles = read<uint16_t>(file);
+                    uint16_t num_fmvs = read<uint16_t>(file);
+                    uint16_t num_cutscenes = read<uint16_t>(file);
+                    uint16_t num_demos = read<uint16_t>(file);
+                    skip(file, 36);
+                    uint16_t flags = read<uint16_t>(file);
+                    skip(file, 6);
+                    uint8_t xorkey = read<uint8_t>(file);
+                    skip(file, 7);
+
+                    num_demos;
+
+                    if (!(flags & 0x100))
+                    {
+                        xorkey = 0;
+                    }
+
+                    auto read_string_list = [&](int count)
+                        {
+                            auto offsets = read_vector<uint16_t>(file, count);
+                            uint16_t total_size = read<uint16_t>(file);
+                            auto data = read_vector<uint8_t>(file, total_size)
+                                | std::views::transform([=](auto x) -> uint8_t { return x ^ xorkey; })
+                                | std::ranges::to<std::vector>();
+                            return offsets
+                                | std::views::transform([&](auto off) { return std::string(reinterpret_cast<char*>(&data[off])); })
+                                | std::ranges::to<std::vector>();
+                        };
+
+                    const auto level_names = read_string_list(num_levels);
+                    const auto chapters = read_string_list(num_chapters);
+                    const auto titles = read_string_list(num_titles);
+                    const auto fmvs = read_string_list(num_fmvs);
+                    const auto level_paths = read_string_list(num_levels) | std::views::transform(to_lowercase) | std::ranges::to<std::vector>();
+                    const auto cutscenes = read_string_list(num_cutscenes);
+
+                    const auto lower_filename = to_lowercase(level_path.filename().string());
+                    const auto found_index = std::ranges::find_if(level_paths, [&](auto& f) { return f.contains(lower_filename); });
+                    if (found_index != level_paths.end())
+                    {
+                        return level_names[found_index - level_paths.begin()];
+                    }
+                }
+                catch(...)
+                {
+                }
+            }
+            return std::nullopt;
+        }
+
+        std::optional<std::string> read_tombpc_4_5(const std::shared_ptr<IFiles>&, const std::string&)
+        {
+            return std::nullopt;
+        }
+    }
+
     ILevelNameLookup::~ILevelNameLookup()
     {
     }
@@ -32,8 +124,7 @@ namespace trview
             if (platform_and_version.remastered)
             {
                 const std::filesystem::path level_path{ level_ptr->filename() };
-                const auto strings_path = level_path.parent_path() /= "../TEXT/EN/STRINGS.TXT";
-                if (const auto strings_data = _files->load_file(strings_path))
+                if (const auto strings_data = load_strings_data(_files, level_path))
                 {
                     std::string level_stem = level_path.stem().string();
                     std::erase(level_stem, '_');
@@ -59,11 +150,17 @@ namespace trview
                     break;
                 case LevelVersion::Tomb2:
                 case LevelVersion::Tomb3:
-                    // TODO: TOMBPC.DAT
+                    if (auto name = read_tombpc_1_3(_files, level_ptr->filename()))
+                    {
+                        return name.value();
+                    }
                     break;
                 case LevelVersion::Tomb4:
                 case LevelVersion::Tomb5:
-                    // TODO: ENGLISH.DAT
+                    if (auto name = read_tombpc_4_5(_files, level_ptr->filename()))
+                    {
+                        return name.value();
+                    }
                     break;
                 }
             }
