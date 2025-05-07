bea3e8d5d03a819fe84dc69b3670b4dd8df2bf83
chreden <4263940+chreden@users.noreply.github.com>
2025-05-07 12:32:10 +0100

    Revert "Add support for TR5 October version"
    This reverts commit 433ca0745d978eba893a96a7e021f67795adf146.

--- trview.app/Windows/Pack/PackWindow.h
+++ trview.app/Windows/Pack/PackWindow.h
@@ -7,7 +7,6 @@ namespace trview
     class PackWindow final : public IPackWindow
     {
     public:
-        explicit PackWindow(const std::shared_ptr<IFiles>& files, const std::shared_ptr<IDialogs>& dialogs);
         virtual ~PackWindow() = default;
         void render() override;
         void set_number(int32_t number) override;
@@ -18,7 +17,5 @@ namespace trview
         std::weak_ptr<trlevel::IPack> _pack;
         std::string _id{ "Pack 0" };
         int32_t _index{ 0u };
-        std::shared_ptr<IFiles> _files;
-        std::shared_ptr<IDialogs> _dialogs;
     };
 }
