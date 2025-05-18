b81eb8f4bb4f01c3c97199e2939bac3e996d0528
chreden <4263940+chreden@users.noreply.github.com>
2025-05-18 21:21:29 +0100

    Start on mapping music ids

--- trview.app/Windows/TriggersWindow.cpp
+++ trview.app/Windows/TriggersWindow.cpp
@@ -18,7 +18,86 @@ namespace trview
                 return room->sector(static_cast<int32_t>(pos.x), static_cast<int32_t>(pos.z)).lock();
             }
             return {};
-        };
+        }
+
+        uint16_t soundtrack_id_to_file(uint16_t index)
+        {
+            switch (index)
+            {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                break;
+            case 6:
+                return 5;
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+                break;
+            case 28:
+                return 24;
+            case 29:
+                return 25;
+            case 30:
+            case 31:
+            case 32:
+            case 33:
+            case 34:
+            case 35:
+            case 36:
+            case 37:
+            case 38:
+            case 39:
+            case 40:
+            case 41:
+            case 42:
+            case 43:
+            case 44:
+            case 45:
+            case 46:
+            case 47:
+            case 48:
+            case 49:
+            case 50:
+            case 51:
+            case 52:
+            case 53:
+            case 54:
+            case 55:
+            case 56:
+            case 57:
+            case 58:
+            case 59:
+            case 60:
+            case 61:
+            case 62:
+            case 63:
+            case 64:
+            case 65:
+                break;
+            }
+            return index;
+        }
     }
 
     ITriggersWindow::~ITriggersWindow()
@@ -431,7 +510,15 @@ namespace trview
                     ImGui::TableNextColumn();
                     ImGui::Text(command_type_name(command.type()).c_str());
                     ImGui::TableNextColumn();
-                    ImGui::Text(std::to_string(command.index()).c_str());
+                    // TODO: Check for TR2?
+                    if (command.type() == TriggerCommandType::PlaySoundtrack)
+                    {
+                        ImGui::Text(std::format("{} ({})", command.index(), soundtrack_id_to_file(command.index())).c_str());
+                    }
+                    else
+                    {
+                        ImGui::Text(std::to_string(command.index()).c_str());
+                    }
                     ImGui::TableNextColumn();
                     ImGui::Text(get_command_display(command).c_str());;
                     if (any_extra)
