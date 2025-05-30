03d72db9ac420b34024050687e0664bc1da641fc
chreden <4263940+chreden@users.noreply.github.com>
2025-05-30 21:27:36 +0100

    Update tests

--- trview.app/Mocks/Graphics/ILevelTextureStorage.h
+++ trview.app/Mocks/Graphics/ILevelTextureStorage.h
@@ -10,6 +10,7 @@ namespace trview
         {
             MockLevelTextureStorage();
             virtual ~MockLevelTextureStorage();
+            MOCK_METHOD(void, add_texture, (const std::vector<uint32_t>&, uint32_t, uint32_t), (override));
             MOCK_METHOD(graphics::Texture, coloured, (uint32_t), (const, override));
             MOCK_METHOD(graphics::Texture, lookup, (const std::string&), (const, override));
             MOCK_METHOD(void, store, (const std::string&, const graphics::Texture&), (override));
@@ -18,6 +19,7 @@ namespace trview
             MOCK_METHOD(graphics::Texture, untextured, (), (const, override));
             MOCK_METHOD(DirectX::SimpleMath::Vector2, uv, (uint32_t, uint32_t), (const, override));
             MOCK_METHOD(uint32_t, tile, (uint32_t), (const, override));
+            MOCK_METHOD(uint32_t, num_textures, (), (const, override));
             MOCK_METHOD(uint32_t, num_tiles, (), (const, override));
             MOCK_METHOD(uint16_t, attribute, (uint32_t), (const, override));
             MOCK_METHOD(DirectX::SimpleMath::Color, palette_from_texture, (uint32_t), (const, override));
