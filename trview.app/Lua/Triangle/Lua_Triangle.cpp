245d04d55c78aca80b4d37c7fa49d88f1d80e496
chreden <4263940+chreden@users.noreply.github.com>
2026-08-05 00:09:36 +0100

    Some triangle bindings

--- trview.app/Lua/Triangle/Lua_Triangle.cpp
+++ trview.app/Lua/Triangle/Lua_Triangle.cpp
@@ -17,7 +17,71 @@ namespace trview
 
             int triangle_index(lua_State* L)
             {
-                L;
+                const auto& self = get_userdata<Triangle>(L, 1);
+                const std::string key = lua_tostring(L, 2);
+
+                if (key == "animation_mode")
+                {
+                    lua_pushinteger(L, static_cast<int>(self.animation_mode));
+                    return 1;
+                }
+                else if (key == "collision_mode")
+                {
+                    lua_pushinteger(L, static_cast<int>(self.collision_mode));
+                    return 1;
+                }
+                else if (key == "colours")
+                {
+                }
+                else if (key == "current_frame")
+                {
+                    lua_pushinteger(L, self.current_frame);
+                    return 1;
+                }
+                else if (key == "current_time")
+                {
+                    lua_pushnumber(L, self.current_time);
+                    return 1;
+                }
+                else if (key == "frames")
+                {
+                }
+                else if (key == "frame_time")
+                {
+                    lua_pushnumber(L, self.frame_time);
+                    return 1;
+                }
+                else if (key == "normal")
+                {
+                }
+                else if (key == "normals")
+                {
+                }
+                else if (key == "position")
+                {
+                }
+                else if (key == "side_mode")
+                {
+                }
+                else if (key == "texture")
+                {
+                }
+                else if (key == "texture_mode")
+                {
+                }
+                else if (key == "transform")
+                {
+                }
+                else if (key == "transparency_mode")
+                {
+                }
+                else if (key == "uv")
+                {
+                }
+                else if (key == "vertices")
+                {
+                }
+
                 return 0;
             }
 
